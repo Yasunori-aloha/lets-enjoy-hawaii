edd71d2a5459281cc64432485f71bf63e8301c11
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-10-19 23:24:32 +0900

    モデル同士の関連付け

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,6 +1,7 @@
 class Area < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :island
+  has_many :experiences
 
   validates :island, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :image
+  has_many :reviews, dependent: :destroy
+  has_many :histories, dependent: :destroy
+  has_many :his_exps, through: :histories, source: :experience
+  has_many :favorites, dependent: :destroy
+  has_many :fav_exps, through: :favorites, source: :experience
 end
