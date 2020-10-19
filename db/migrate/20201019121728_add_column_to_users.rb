118365785eab71260a0f791116343840784d74ce
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-10-19 21:46:47 +0900

    usersテーブル作成

---
+++ db/migrate/20201019121728_add_column_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :name, :string, null: false, default: ""
+    add_column :users, :introduce, :text
+    add_column :users, :admin, :integer
+  end
+end
