87de777e5706d86fabd6b492631bbdef34070522
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-10-25 19:14:06 +0900

    tops/index.html.hamlの検索フォームを'ransack'のメソッドに書き換え実施 #18

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,11 @@
 class TopsController < ApplicationController
+  before_action :set_search
+
   def index
+  end
+
+  def set_search
+    @search = Experience.ransack(params[:q])
+    @items = @search.result
   end
 end
