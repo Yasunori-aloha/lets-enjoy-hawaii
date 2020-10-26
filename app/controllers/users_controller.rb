7038d3cf2806ef60f3dacd2670c66d4fc647d5f0
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-10-27 00:25:18 +0900

    ログインユーザーでも他人のマイページには遷移できない様にusersコントローラーを修正 #19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
-
+  
   def show
-    @user = User.find(params[:id])
+    unless current_user.id == params[:id]
+      redirect_to root_path
+    else
+      @user = User.find(params[:id])
+    end
   end
 end
