a1774f39afa1d19a0d86844f85e40102e53821a8
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-10-27 00:43:20 +0900

    params[:id]がstring型だったため、integer型へ変換する様にコード修正 #19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
   
   def show
-    unless current_user.id == params[:id]
+    unless current_user.id == params[:id].to_i
       redirect_to root_path
     else
       @user = User.find(params[:id])
