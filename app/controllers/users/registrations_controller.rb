7e66679980079e9c39289c611267fbff11ecb727
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-01 18:55:24 +0900

    'update'アクションの際に':name'と':introduce'のキーを更新できる様にpermit実施 #30

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   def new
     @user = User.new
@@ -54,10 +54,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduce])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
