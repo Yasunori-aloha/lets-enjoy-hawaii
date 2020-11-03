9ae4084c2c084706a84e2a7fb084ceb19210de49
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-03 22:41:14 +0900

    'experirencesコントローラー'の'showアクション'を定義し、'show.html.haml'内にインスタンスを受渡しする為のコードを記述 #40

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -2,7 +2,10 @@ class ExperiencesController < ApplicationController
   def search
   end
 
-
+  def show
+    @experience = Experience.find(params[:id])
+  end
+  
   def edit
     # クリックされたカテゴリーの'data-id'を基にGenreモデルを探し、結果に紐づくお店やレジャー施設情報をインスタンスに格納。
     @genres = Genre.where(category_id: params[:data][:id])
