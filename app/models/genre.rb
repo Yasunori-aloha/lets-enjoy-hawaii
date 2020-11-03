90a7812546cf7593edad158e9f14c2f05b15f918
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-03 16:24:49 +0900

    'Typeモデル'を'ActiveHash'で定義し、'Genreモデル'との関連付け実施 #40

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :type
   has_many :experiences
 end
---
+++ app/models/type.rb
@@ -0,0 +1,10 @@
+class Island < AcriveHash::Base
+  self.data = [
+    { id: 1, name: 'ホテル・コンドミニアム'},
+    { id: 2, name: '食事'}
+    { id: 3, name: 'レンタカー'}
+    { id: 4, name: '遊び・体験'}
+    { id: 5, name: '観光地'}
+    { id: 6, name: 'ショッピング'}
+  ]
+end
