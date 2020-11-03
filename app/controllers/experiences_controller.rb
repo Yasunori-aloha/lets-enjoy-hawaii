8899a36921a926df370ab7334221da3339b8765b
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-03 21:01:14 +0900

    'experiences#edit'にリクエストが飛ぶと'Experienceモデル'内にある'ショッピングカテゴリー'の要素をインスタンスで返す様にコード記述 #40

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -2,7 +2,13 @@ class ExperiencesController < ApplicationController
   def search
   end
 
+
   def edit
+    @genres = Genre.where(type_id: 6)
+    @experiences = []
+    @genres.each do |genre|
+      @experiences << Experience.find_by(genre_id: genre.id)
+    end
     render "search/#{params[:name]}"
   end
 end
