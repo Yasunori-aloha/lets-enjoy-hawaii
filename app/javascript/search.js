c95f1e09afe207e75cfe3f5a917ddaeac3aa0fc9
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-07 19:04:34 +0900

    複数件要素があっても、全てに星の5段階評価を適用させて表示する様に'search.js'を変更 #44

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -9,19 +9,21 @@ $(window).on('load',function(){
     'assets/landmark_icon.png',
     'assets/shopping_icon.png'
   ];
-  // 'category_id'を取得して、その'id'に対応したアイコン画像を配列から取得して表示させる。
-  const categoryIcon = $('#category_icon')[0];
-  const categoryId = $('#category_id')[0].innerText;
-  categoryIcon.src = categoryIcons[categoryId - 1];
-
-  // URLのパスを取得して、値が一致すれば関数を動作させる。
   const url = location.pathname;
-  const conditions = (url == '/shopping') || (url == '/landmark') || (url == '/leisure') || (url == '/rentacar') || (url == '/dinner') || (url == '/hotel')
+  const conditions = (url == '/shopping') || (url == '/landmark') || (url == '/leisure') || (url == '/rentacar') || (url == '/dinner') || (url == '/hotel');
   if(conditions) {
-    // exp.scoreの点数を取得して、変数に代入。
-    const expScore = $('#exp_score')[0].innerText
-    // `expScore`に'20'を乗算することにより'%'の数値にして、`star_rating`の'width'プロパティの値に設定する。
-    const starRatingPersent = expScore * 20;
-    $('#star_rating').width(`${starRatingPersent}%`);
+    // 'category_id'を取得して、その'id'に対応したアイコン画像を配列から取得して表示させる。
+    const categoryIcon = $('#category_icon')[0];
+    const categoryId = $('#category_id')[0].innerText;
+    categoryIcon.src = categoryIcons[categoryId - 1];
+
+    // exp.scoreクラスを複数取得して、ノードリストから配列へ変換し、各要素毎に処理をしていく。
+    const expScoreArray = Array.from($('.exp_score'));
+    expScoreArray.forEach((element, index) => {
+      const expScore = element.innerText;
+      // 'expScore'に'20'を乗算することにより'%'の数値にして、`star_rating`の'width'プロパティの値に設定する。
+      const starRatingPersent = expScore * 20;
+      $(`#star_rating${index}`).css('width', `${starRatingPersent}%`);
+    });
   }
 });
