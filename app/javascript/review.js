070b82325857922bba47dd8c0b2a98e009b71a3f
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-13 19:34:19 +0900

    口コミ本文入力欄の項目名表示を実装 #54

--- app/javascript/review.js
+++ app/javascript/review.js
@@ -2,6 +2,9 @@
 
 $(function(){
   $('[id^="review_score_"]').on('click', function(){
+    console.log($(this)[0].innerText);
+  });
+  $('[id^="star_btn"]').on('mouseover', function(){
     console.log($(this));
   });
 });
