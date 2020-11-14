00ab11df34a778727bc40a647dadb525d3195c29
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-14 19:13:07 +0900

    アクティビティへの口コミ投稿機能実装 #54

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,12 +3,14 @@ class ReviewsController < ApplicationController
 
   def new
     @review_image = ReviewImage.new
+    binding.pry
   end
 
   def create
     @review_image = ReviewImage.new(review_params)
     if @review_image.valid?
-      binding.pry
+      @review_image.save
+      redirect_to experience_path(params[:experience_id])
     else
       render action: :new
     end
@@ -17,7 +19,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review_image).permit(:title, :comment, :score, images: [])
+    params.require(:review_image).permit(:title, :comment, :score, images: []).merge(user_id: current_user.id, experience_id: @experience.id)
   end
 
   def find_experience
--- app/models/review_image.rb
+++ app/models/review_image.rb
@@ -1,6 +1,6 @@
 class ReviewImage
   include ActiveModel::Model
-  attr_accessor :score, :title, :comment, :images
+  attr_accessor :score, :title, :comment, :images, :user_id, :experience_id
 
   with_options presence: true do
     validates :title
@@ -9,7 +9,8 @@ class ReviewImage
   end
 
   def save
-    Review.create(title: title, comment: comment, score: score, user_id: current_user.id, experience_id: experience_id)
-    Experience.update!()
+    Review.create(title: title, comment: comment, score: score, user_id: user_id, experience_id: experience_id)
+    average_score = (Experience.find(experience_id).reviews.sum(:score).to_f / Experience.find(experience_id).reviews.length).round(1)
+    Experience.find(experience_id).update(images: images, score: average_score)
   end
 end
