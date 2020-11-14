d5acf498538addc6893a382e31f06441034ff497
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-14 21:46:25 +0900

    口コミ投稿ページの'☆'マークをホバーすると表示が切り替わり、現在選択中の評価点を可視化できる様に'review.js'にコード記述 #54

--- app/javascript/review.js
+++ app/javascript/review.js
@@ -1,10 +1,21 @@
 'use strict';
 
 $(function(){
-  $('[id^="review_score_"]').on('click', function(){
-    console.log($(this)[0].innerText);
-  });
+  // '★'をホバーするとホバーした位置から左側にある'★'を全て表示変更させる。
   $('[id^="star_btn"]').on('mouseover', function(){
-    console.log($(this));
+    const starId = $(this)[0].id.slice(-1)
+    for (let index = starId; index > 0; index--){
+      $(`#star_btn${index}`)[0].innerText = '★';
+    }
+  });
+  $('[id^="star_btn"]').on('mouseout', function(){
+    const starId = $(this)[0].id.slice(-1)
+    for (let index = starId; index > 0; index--){
+      $(`#star_btn${index}`)[0].innerText = '☆';
+    }
+  });
+  // '★'をクリックすると、クリックした位置から左側の'★'が選択状態となり、ホバーを外しても表示が変更されたままになる。
+  $('[id^="review_score_"]').on('click', function(){
+    console.log($(this)[0].value);
   });
 });
