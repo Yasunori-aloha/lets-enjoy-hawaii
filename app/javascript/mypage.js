490580e23cee06780b7734d1a5bea5c0107d1c6a
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-14 23:48:34 +0900

    添付画像が複数なら、その画像の枚数だけ処理を繰り返す様に'mypage.js'のコードを修正 #54

--- app/javascript/mypage.js
+++ app/javascript/mypage.js
@@ -2,13 +2,15 @@
 
 $(document).on('change', '#user_image, #review_images', function(e){
   // 変更するために選択した画像ファイルを変数に代入する。
-  const imageFile = e.target.files[0];
-  const fileReader = new FileReader();
-
-  // 画像ファイルをurlにエンコードする。
-  fileReader.readAsDataURL(imageFile);
-  console.log(e.target.files);
-  fileReader.onload = () => {
-    $(`#${this.id}_set`).attr('src', fileReader.result);
+  const imageFile = e.target.files;
+  
+  // 画像ファイルの数だけ、処理を繰り返す
+  for (let index = 0; index < e.target.files.length; index++) {
+    const fileReader = new FileReader();
+    // 画像ファイルをurlにエンコードする。
+    fileReader.onload = () => {
+      $(`#${this.id}_set`).attr('src', fileReader.result);
+    };
+    fileReader.readAsDataURL(imageFile[index]);
   };
 });
