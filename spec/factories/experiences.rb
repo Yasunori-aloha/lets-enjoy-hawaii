2b57c2962923f0cba2e505e14e43df57283cc8fe
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-17 01:14:37 +0900

    'Reviewモデル'用のFactoryBotを記述 #54

---
+++ spec/factories/experiences.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :experience do
+    
+  end
+end
--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :review do
-    
+    title {Faker::Lorem.word}
+    comment {Faker::Lorem.sentence}
+    score {rand(1..4)}
+    association :user
+    association :experience
+
+    after(:build) do |review|
+      reivew.image.attach(io: File.open('public/テスト画像.png')), filename: 'テスト画像'
   end
 end
