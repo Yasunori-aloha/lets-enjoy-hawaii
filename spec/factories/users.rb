1005213c8ff5713eaed36fc2eba749afb580e86d
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-17 01:20:30 +0900

    'Userモデル'用のFactoryBotを記述 #54

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :user do
-    
+    name {Faker::Name.initials(number:5)}
+    email {Faker::Internet.free_email}
+    introduce {Faker::Lorem.sentence}
+    admin {0}
+    password {Faker::Internet.password}
+    password_confirmation {password}
+
+    after(:build) do |user|
+      user.image.attach(io: File.open('public/テスト画像.png')), filename: 'テスト画像'
+    end
   end
 end
