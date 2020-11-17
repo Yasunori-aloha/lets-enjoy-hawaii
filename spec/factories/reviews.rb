b3590cc5debbe4314507c351afcb52e3563baf05
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-17 23:41:57 +0900

    口コミが正常に投稿できた場合の結合テストコード記述 #54

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :review do
     title {Faker::Lorem.word}
     comment {Faker::Lorem.sentence}
-    score {rand(1..5)}
+    score {rand(1..5).to_f}
 
     association :user
     association :experience
--- spec/system/reviews_spec.rb
+++ spec/system/reviews_spec.rb
@@ -5,11 +5,41 @@ RSpec.describe "Reviews", type: :system do
     @user = create(:user)
     @experience = create(:experience)
     @review = build(:review)
-    @review.images = [fixture_file_upload('files/test_image.png', 'image/png')]
   end
 
   context '口コミの新規投稿ができる場合' do
     it 'ログインしているユーザーで、正常な値を入力していれば新規投稿ができる' do
+      #ログインする
+      visit new_user_session_path
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      click_on 'ログイン'
+      expect(current_path).to eq root_path
+      
+      # アクティビティ詳細ページへ移動して、口コミ投稿ボタンがあるか確認する
+      visit experience_path(@experience.id)
+      expect(page).to have_content('口コミ投稿')
+      
+      # 口コミ投稿ページへ移動してフォームに情報を入力する。
+      visit new_experience_review_path(@experience.id)
+      find_by_id("star_btn#{rand(1..5)}").click
+      fill_in 'review_title', with: @review.title
+      fill_in 'review_comment', with: @review.comment
+      find('input[name="review[images][]"]', visible: false).set("#{Rails.root}/spec/fixtures/files/test_image.png")
+      
+      # フォームを送信すると、Reviewモデルのレコード数が1上がり、アクティビティの評価点が更新されて詳細ページへリダイレクトされる。
+      expect{ click_on('投稿する')}.to change {Review.count}.by(1)
+      expect(@experience.reload.score).to eq @experience.reviews.average(:score).round(1)
+      expect(current_path).to eq experience_path(@experience.id)
+      
+      # 詳細ページの口コミ件数が1件増えている。
+      reviews = all(:css, '[data-name="review_count"]')
+      reviews.each do |review|
+        expect(review).to have_content(@experience.reviews.length.to_s)
+      end
+
+
+      binding.pry
     end
   end
   context '口コミの新規投稿ができない場合' do
