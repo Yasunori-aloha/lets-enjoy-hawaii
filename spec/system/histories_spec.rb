5d697a2304e3523a2fe3070aa9bd3c5eb7409c31
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-18 21:06:25 +0900

    行ったボタンを再度クリックした場合の結合テストコード実装 #57

--- spec/system/histories_spec.rb
+++ spec/system/histories_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Histories", type: :system do
   let(:user) {create(:user)}
   let(:experience) {create(:experience)}
+  let(:history) {build(:history, user_id: user.id, experience_id: experience.id)}
 
   describe '行ったボタンクリック時の動作' do
     context '行った記録がクリックした際にDBに保存される場合' do
@@ -14,8 +15,9 @@ RSpec.describe "Histories", type: :system do
         click_on 'ログイン'
         expect(current_path).to eq root_path
 
-        # アクティビティ詳細ページへ移動して、行ったボタンがあるか確認する。
+        # アクティビティ詳細ページへ移動して、行ったボタンがあり、アイコンが足跡か確認する。
         visit experience_path(experience.id)
+        expect(page).to have_selector('.fa-shoe-prints')
         expect(page).to have_content('行った')
 
         # 行ったボタンをクリックするとHistoryモデルのレコード数が1上がり、足跡アイコンがチェックマークに変化する。
@@ -25,6 +27,22 @@ RSpec.describe "Histories", type: :system do
     end
     context '行った記録がクリックした際にDBから消去される場合' do
       it 'ログインユーザーですでに記録が保存されているアクティビティではクリックするとDBから記録が消去される。' do
+        # ログインする。
+        visit new_user_session_path
+        fill_in 'user_email', with: user.email
+        fill_in 'user_password', with: user.password
+        click_on 'ログイン'
+        expect(current_path).to eq root_path
+
+        # アクティビティ詳細ページへ移動して、行ったボタンがあり、アイコンがチェックマークか確認する。
+        history.save
+        visit experience_path(experience.id)
+        expect(page).to have_selector('.fa-check')
+        expect(page).to have_content('行った')
+
+        # 行ったボタンをクリックするとHistoryモデルのレコード数が1下がり、チェックマークが足跡アイコンに変化する。
+        expect{find('.have_been_btn').click}.to change {History.count}.by(-1)
+        expect(page).to have_selector('.fa-shoe-prints')
       end
     end
     context 'DBに記録が保存も消去もできない場合' do
