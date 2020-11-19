777ab5a869683ab8b41c9409193e6259e71e16c2
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-19 22:42:58 +0900

    口コミ投稿日順でのソート機能実装 #59

--- app/javascript/review.js
+++ app/javascript/review.js
@@ -3,6 +3,7 @@
 $(function(){
 
   let clickedId = 0
+  let reviewList = null
 
   // '★'をクリックした位置から左側の'★'がホバーを外しても表示が変更されたままで、クリックした位置から右側が非選択状態になる。
   $('[id^="review_score_"]').on('click', function(){
@@ -28,13 +29,34 @@ $(function(){
     }
   });
 
+  // 並び替えの'評価順'ボタンをクリックすると、アクティビティの評価点の降順に表示を変更する。
   $('#sort_score').on('click', function(){
-    const reviewList = $('.review_list li');
+    reviewList = $('.review_list li');
     reviewList.sort(function(a,b){
       const aScore = Number($(a).find('.exp_score').text());
       const bScore = Number($(b).find('.exp_score').text());
       return bScore - aScore;
     });
+    $('#sort_created_at').removeClass('active_sort')
+    $('#sort_score').addClass('active_sort')
+    $('.review_list').empty();
+    $('.review_list').append(reviewList);
+  });
+  // 並び替えの'投稿日順'ボタンをクリックすると、アクティビティの投稿日の降順に表示を変更する。
+  $('#sort_created_at').on('click', function(){
+    reviewList = $('.review_list li');
+    reviewList.sort(function(a,b){
+      const aTime = $(a).find('.review_created_at').text();
+      const bTime = $(b).find('.review_created_at').text();
+      if (aTime > bTime) {
+        return -1
+      } else {
+        return 1
+      }
+    });
+    console.log(reviewList);
+    $('#sort_score').removeClass('active_sort')
+    $('#sort_created_at').addClass('active_sort')
     $('.review_list').empty();
     $('.review_list').append(reviewList);
   });
