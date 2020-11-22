7e94e81db5baad4c54cad97470e471500b4cc736
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-22 20:34:17 +0900

    'toggleClass'を使用して、classの追加と削除のコードを1つにまとめる #49

--- app/javascript/review.js
+++ app/javascript/review.js
@@ -1,61 +1,67 @@
-'use strict';
+"use strict";
 
-$(function(){
-
-  let clickedId = 0
-  // 口コミページ表示直後は投稿日順で表示されている為、2を変数に代入している。
-  let activeSortId = 2
+$(function () {
+  let clickedId = 0;
+  // 表示ページに'並び替えボタン'があれば、初期選択されているボタンの'id'を'activeSortId'
+  let activeSortId = null;
+  if ($(".active_sort").length) {
+    activeSortId = $(".active_sort")[0].id.match(/\d/)[0];
+  }
 
   // '★'をクリックした位置から左側の'★'がホバーを外しても表示が変更されたままで、クリックした位置から右側が非選択状態になる。
-  $('[id^="review_score_"]').on('click', function(){
+  $('[id^="review_score_"]').on("click", function () {
     clickedId = Number($(this)[0].value);
-    for (let index = (clickedId + 1); index <= 5; index++){
-      $(`#star_btn${index}`)[0].innerText = '☆';
-    };
+    for (let index = clickedId + 1; index <= 5; index++) {
+      $(`#star_btn${index}`)[0].innerText = "☆";
+    }
   });
   $('[id^="star_btn"]').on({
     // '★'をホバーするとホバーした位置から左側にある'★'を全て表示変更させる。
-    'mouseover': function(){
+    mouseover: function () {
       const starId = $(this)[0].id.slice(-1);
-      for (let index = starId; index > 0; index--){
-        $(`#star_btn${index}`)[0].innerText = '★';
-      };
+      for (let index = starId; index > 0; index--) {
+        $(`#star_btn${index}`)[0].innerText = "★";
+      }
     },
     // '★'のホバーを外すと、クリックされていない部分を'☆'状態に戻す。
-    'mouseout': function(){
+    mouseout: function () {
       const starId = $(this)[0].id.slice(-1);
-      for (let index = starId; index > clickedId; index--){
-        $(`#star_btn${index}`)[0].innerText = '☆';
-      };
-    }
+      for (let index = starId; index > clickedId; index--) {
+        $(`#star_btn${index}`)[0].innerText = "☆";
+      }
+    },
   });
 
-  // 並び替えの'評価順・投稿日順'ボタンをクリックすると、アクティビティの評価点・投稿日の降順に表示を変更する。
-  $('[id*="_sort_"]').on('click', function(){
-    const sortName = this.id.match(/(.*)(?=_sort_)/)[0]
+  // 並び替え用ボタンをクリックすると、ボタンに表示されている項目の降順に表示を変更する。
+  $('[id*="_sort_"]').on("click", function () {
+    const sortName = this.id.match(/(.*)(?=_sort_)/)[0];
     const sortId = Number(this.id.match(/\d/)[0]);
     // すでに選択されている並び順では、動作しない様に否定文で分岐させる。
     if (!(sortId === activeSortId)) {
-      // 表示している口コミの要素一覧を変数に格納する。
+      // 表示している要素一覧を変数に格納する。
       const sortList = $(`.${sortName}_list li`);
-      sortList.sort(function(a,b){
+      sortList.sort(function (a, b) {
         const aText = $(a).find(`.${sortName}_sort_material${sortId}`).text();
         const bText = $(b).find(`.${sortName}_sort_material${sortId}`).text();
         if (aText > bText) {
-          return -1
+          return -1;
         } else {
-          return 1
+          return 1;
         }
       });
-      $(`#${sortName}_sort_${activeSortId}`).removeClass('active_sort').addClass('change_link');
+      $(`#${sortName}_sort_${activeSortId}`)
+        .removeClass("active_sort")
+        .addClass("change_link");
       // 選択した並び順のID数値を再代入している。
       activeSortId = sortId;
-      $(this).addClass('active_sort').removeClass('change_link').removeClass('link_hover');
-      $(`.${sortName}_list`).empty().append(sortList).addClass('active_fade');
-      setTimeout(function(){
-        $(`.${sortName}_list`).removeClass('active_fade');
+      $(this)
+        .addClass("active_sort")
+        .removeClass("change_link")
+        .removeClass("link_hover");
+      $(`.${sortName}_list`).empty().append(sortList).addClass("active_fade");
+      setTimeout(function () {
+        $(`.${sortName}_list`).removeClass("active_fade");
       }, 500);
     }
   });
-
 });
--- app/javascript/search.js
+++ app/javascript/search.js
@@ -1,57 +1,59 @@
-'use strict';
+"use strict";
 
-$(window).on('load',function(){
+$(window).on("load", function () {
   const categoryIcons = [
-    'hotel_icon',
-    'lunch_icon',
-    'car_icon',
-    'leisure_icon',
-    'landmark_icon',
-    'shopping_icon'
+    "hotel_icon",
+    "lunch_icon",
+    "car_icon",
+    "leisure_icon",
+    "landmark_icon",
+    "shopping_icon",
   ];
   const url = location.pathname;
-  const conditions = (url == '/shopping') || (url == '/landmark') || (url == '/leisure') || (url == '/rentacar') || (url == '/dinner') || (url == '/hotel') || (/\/experiences\/[0-9]{1,}/.test(url));
-  if(conditions) {
+  const conditions =
+    url == "/shopping" ||
+    url == "/landmark" ||
+    url == "/leisure" ||
+    url == "/rentacar" ||
+    url == "/dinner" ||
+    url == "/hotel" ||
+    /\/experiences\/[0-9]{1,}/.test(url);
+  if (conditions) {
     // 'category_id'を取得して、その'id'に対応したアイコン画像用のクラスを配列から取得して追加させる。
-    const categoryIcon = $('#category_icon');
-    const categoryId = $('#category_id')[0].innerText;
+    const categoryIcon = $("#category_icon");
+    const categoryId = $("#category_id")[0].innerText;
     categoryIcon.addClass(`${categoryIcons[categoryId - 1]}`);
   }
-  if(conditions) {
+  if (conditions) {
     // exp_scoreクラスを複数取得して、ノードリストから配列へ変換し、各要素毎に処理をしていく。
-    const expScoreArray = Array.from($('.exp_score'));
+    const expScoreArray = Array.from($(".exp_score"));
     expScoreArray.forEach((element, index) => {
       const expScore = element.innerText;
       // 'expScore'に'20'を乗算することにより'%'の数値にして、`star_rating`の'width'プロパティの値に設定する。
       const starRatingPercent = expScore * 20;
-      $(`#star_rating${index}`).css('width', `${starRatingPercent}%`);
+      $(`#star_rating${index}`).css("width", `${starRatingPercent}%`);
     });
   }
   // 詳細ページの5段階評価ごとの'%'を取得して数値に対応したグラフを表示させる。
-  if((/\/experiences\/[0-9]{1,}/.test(url)) && !(/\/experiences\/[0-9]{1,}\//.test(url))) {
+  if (
+    /\/experiences\/[0-9]{1,}/.test(url) &&
+    !/\/experiences\/[0-9]{1,}\//.test(url)
+  ) {
     const showScoreArray = Array.from($('[id*="score_"]'));
     showScoreArray.reverse().forEach((element, index) => {
       const percent = element.innerText.match(/\d+/)[0];
-      $(`#graph_score${index + 1}`).css('width', `${percent}%`);
+      $(`#graph_score${index + 1}`).css("width", `${percent}%`);
     });
   }
 });
 
-$(function(){
-  // 'id名'に'change'と入っている要素をホバーするとリンクの表示を変更させる。
-  $(document).on('mouseover','[class*="change_link"]',function(){
-    $(this).addClass('link_hover');
-  }),
-  $(document).on('mouseout','[class*="change_link"]',function(){
-    $(this).removeClass('link_hover');
+$(function () {
+  // 'id名'に'change'と入っている要素をホバーしている時だけリンクの表示を変更させる。
+  $(document).on("mouseover mouseout", '[class*="change_link"]', function () {
+    $(this).toggleClass("link_hover");
   });
   // 'ボタン'にホバーすると表示色を変更させる。
-  $('[id^="btn"]').on('mouseover',function(){
-    $(this).addClass('btn_hover');
+  $('[id^="btn"]').on("mouseover mouseout", function () {
+    $(this).toggleClass("btn_hover");
   });
-  $('[id^="btn"]').on('mouseout',function(){
-    $(this).removeClass('btn_hover');
-  });
-
-  $()
 });
