f3cbf9c285ed495d5bcfbcdaef7c30eff03f5568
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-25 20:05:55 +0900

    'favorites#create・favorites#destroy'のレスポンス形式をjson形式へ変更 #55

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,12 +3,14 @@
 class FavoritesController < ApplicationController
   def create
     # 『お気に入り』ボタンをクリックしたログインユーザーの'id'とボタンが設置されていた'experience'の'id'を引数にインスタンスを生成する。
-    current_user.favorites.create(experience_id: params[:experience_id])
-    redirect_experience(params[:experience_id])
+    favorite = current_user.favorites.create(experience_id: params[:experience_id])
+    render json:{ favorite: favorite }
+    # redirect_experience(params[:experience_id])
   end
 
   def destroy
-    current_user.favorites.find_by(experience_id: params[:experience_id]).destroy
-    redirect_experience(params[:experience_id])
+    favorite = current_user.favorites.find_by(experience_id: params[:experience_id]).destroy
+    render json:{ favorite: favorite }
+    # redirect_experience(params[:experience_id])
   end
 end
