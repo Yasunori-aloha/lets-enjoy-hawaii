bebee1679076b05f383686df2cfd1e7ef7181e7d
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-25 21:50:48 +0900

    'link_toメソッド'の'remote: true'オプションでAJAX通信をON #55

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,9 +3,7 @@
 class FavoritesController < ApplicationController
   def create
     # 『お気に入り』ボタンをクリックしたログインユーザーの'id'とボタンが設置されていた'experience'の'id'を引数にインスタンスを生成する。
-    # favorite =
-    #   current_user.favorites.create(experience_id: params[:experience_id])
-    # render json: { favorite: favorite }
+    current_user.favorites.create(experience_id: params[:experience_id])
     # redirect_experience(params[:experience_id])
   end
 
@@ -13,7 +11,7 @@ class FavoritesController < ApplicationController
     favorite =
       current_user.favorites.find_by(experience_id: params[:experience_id])
         .destroy
-    render json: { favorite: favorite }
+    # render json: { favorite: favorite }
     # redirect_experience(params[:experience_id])
   end
 end
