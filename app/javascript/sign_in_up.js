29564e656ddd926d80a9ff99475f7f1207236439
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-27 21:48:22 +0900

    入力フォームのフォーカスが外れた時の処理を'sign_in_up.js'に記述 #32

--- app/javascript/sign_in_up.js
+++ app/javascript/sign_in_up.js
@@ -1,1 +1,13 @@
 'use strict';
+
+$(function(){
+  // 会員登録入力フォームのフォーカスが外れた時にバリデーションが正しいが判定する関数。
+  $('.signin_up_input').on('blur', function(){
+    // console.log(!(this.value));
+    if (!(this.value)) {
+      $(this).parent().append('<span class="signin_up_error_message">この項目は必須です</span>');
+    } else {
+      $(this).next().remove();
+    }
+  });
+});
