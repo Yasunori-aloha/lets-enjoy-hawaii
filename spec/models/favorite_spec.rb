bb1472a905b033bd961e05569c8ccaf787bac31f
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-28 15:55:37 +0900

    'Favoriteモデル'のテストコード実装 #66

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -10,11 +10,15 @@ RSpec.describe Favorite, type: :model do
     context 'お気に入り登録ができる場合' do
       it 'ユーザーがまだお気に入り登録していないアクティビティなら登録できる' do
         expect(favorite).to be_valid
-        # binding.pry
       end
     end
     context 'お気に入り登録ができない場合' do
       it 'ユーザーが既にお気に入り登録しているアクティビティなら登録できない' do
+        favorite.save
+        another_favorite.user_id = favorite.user_id
+        another_favorite.experience_id = favorite.experience_id
+        another_favorite.valid?
+        expect(another_favorite.errors.full_messages).to include 'Userはすでに存在します'
       end
     end
   end
