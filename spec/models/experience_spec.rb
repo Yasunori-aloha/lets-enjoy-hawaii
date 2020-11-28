0f56296aec0041070efbc8bfa913f8fe6e715127
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-28 16:22:25 +0900

    'Experienceモデル'の正常系テストコード実装 #66

--- spec/models/experience_spec.rb
+++ spec/models/experience_spec.rb
@@ -3,19 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe Experience, type: :model do
+  let(:experience) { build(:experience) }
+
   describe 'アクティビティ登録' do
     context 'アクティビティが登録できる場合' do
       it '必要な値が全て入力されている' do
+        expect(experience).to be_valid
+        # binding.pry
       end
       it 'outlineが入力されていなくても登録できる' do
+        experience.outline = nil
+        expect(experience).to be_valid
       end
       it 'business_hours_startが入力されていなくても登録できる' do
+        experience.business_hours_start = nil
+        expect(experience).to be_valid
       end
       it 'business_hours_finishが入力されていなくても登録できる' do
+        experience.business_hours_finish = nil
+        expect(experience).to be_valid
       end
       it 'scoreが入力されていなくても登録できる' do
+        experience.score = nil
+        expect(experience).to be_valid
       end
       it '画像が添付されていなくても登録できる' do
+        experience.image = nil
+        expect(experience).to be_valid
       end
     end
     context 'アクティビティが登録できない場合' do
@@ -30,20 +44,6 @@ RSpec.describe Experience, type: :model do
       it 'area_idが入力されていない' do
       end
       it 'genre_idが入力されていない' do
-      end
-      it 'nameが入力されていない' do
-      end
-      it 'nameが入力されていない' do
-      end
-      it 'nameが入力されていない' do
-      end
-      it 'nameが入力されていない' do
-      end
-      it 'nameが入力されていない' do
-      end
-      it 'nameが入力されていない' do
-      end
-      it 'nameが入力されていない' do
       end
     end
   end
