ce3e3c853d7a1541bcec67e07ec052faff9e552e
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-28 15:39:50 +0900

    'Areaモデル'のexampleを記述 #66

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe Area, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:area) { build(:area) }
+
+  describe 'ジャンル登録' do
+    context 'ジャンル登録ができる場合' do
+      it 'name・category_idが入力されていると登録できる' do
+      end
+    end
+    context 'ジャンル登録ができない場合' do
+      it 'nameが入力されていないと登録できない' do
+      end
+      it 'category_idが選択されていないと登録できない' do
+      end
+    end
+  end
 end
