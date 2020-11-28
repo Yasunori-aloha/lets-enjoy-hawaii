76c3da2d5f2197abffba5c17b9c0b8919e396c3d
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-28 14:30:32 +0900

    新規登録ができた場合とできない場合の分岐を記述 #66

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  describe 'ユーザー新規登録' do
+    context 'ユーザーの新規登録ができる場合' do
+      it '' do
+      end
+    end
+    context 'ユーザーの新規登録ができない場合' do
+      it '' do
+      end
+    end
+  end
 end
