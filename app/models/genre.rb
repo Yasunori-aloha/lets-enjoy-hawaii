3740d44b159b7419a531cdfaacba781a722661f2
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-28 15:35:39 +0900

    'Genreモデル'のバリデーション追記 #66

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,4 +4,8 @@ class Genre < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   has_many :experiences
+
+  with_options presence: true do
+    validates :name, :category_id
+  end
 end
--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -9,13 +9,16 @@ RSpec.describe Genre, type: :model do
     context 'ジャンル登録ができる場合' do
       it 'name・category_idが入力されていると登録できる' do
         expect(genre).to be_valid
-        # binding.pry
       end
     end
     context 'ジャンル登録ができない場合' do
       it 'nameが入力されていないと登録できない' do
+        genre.name = nil
+        genre.valid?
+        binding.pry
       end
       it 'category_idが選択されていないと登録できない' do
+        # genre.ca
       end
     end
   end
