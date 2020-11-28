3263333a404902a64f0e8f6faf82e277ae8c1aaa
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-28 15:47:39 +0900

    'Favoriteモデル'のexampleを記述 #66

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -3,5 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:favorite) { build(:favorite) }
+
+  describe 'アクティビティお気に入り登録' do
+    context 'お気に入り登録ができる場合' do
+      it 'ユーザーがまだお気に入り登録していないアクティビティなら登録できる' do
+      end
+    end
+    context 'お気に入り登録ができない場合' do
+      it 'ユーザーが既にお気に入り登録しているアクティビティなら登録できない' do
+      end
+    end
+  end
 end
