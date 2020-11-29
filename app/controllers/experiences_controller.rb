53b4cbe3de1960bd2d96d238a474692d5aa4fab9
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-11-29 17:24:27 +0900

    '島名・地図検索'を選択した場合でも、アクティビティを取得できる様に'experienceコントローラー'のコード修正 #68

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -12,15 +12,25 @@ class ExperiencesController < ApplicationController
   end
 
   def edit
-    binding.pry
     # クリックされたカテゴリーの'name'を基にCategoryモデルを探し、結果に紐づくお店やレジャー施設情報をインスタンスに格納。
-    @category = Category.find_by(search: params[:name])
-    @experiences = []
-    Genre.where(category_id: @category.id).find_each do |genre|
-      Experience.includes(:favorites).includes(:genre).includes(:area).where(
-        genre_id: genre.id
-      ).find_each { |exp| @experiences << exp }
+    if params[:class] == 'category'
+      @category = Category.find_by(search: params[:name])
+      @experiences = []
+      Genre.where(category_id: @category.id).find_each do |genre|
+        Experience.includes([:favorites, :genre, :area]).where(
+          genre_id: genre.id
+        ).find_each { |exp| @experiences << exp }
+      end
+      render 'experiences/category'
+    elsif params[:class] == 'island'
+      @island = Island.find_by(search: params[:name])
+      @experiences = []
+      Area.where(island_id: @island.id).find_each do |area|
+        Experience.includes([:favorites, :genre, :area]).where(
+          area_id: area.id
+        ).find_each { |exp| @experiences << exp }
+      end
+      # binding.pry
     end
-    render 'experiences/category'
   end
 end
