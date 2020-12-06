f3bd8ceed2522dea5d7f22484d12d774c1b6e0db
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-12-06 18:09:39 +0900

    'historiesコントローラー'に一覧表示用の'indexアクション'とコメント保存用の'updateアクション'を定義 #78

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class HistoriesController < ApplicationController
+
+  def index
+
+  end
+
   def create
     find_exp_id(params[:experience_id])
     current_user.histories.create(experience_id: params[:experience_id])
@@ -10,5 +15,9 @@ class HistoriesController < ApplicationController
     find_exp_id(params[:experience_id])
     current_user.histories.find_by(experience_id: params[:experience_id])
                 .destroy
+  end
+
+  def update
+
   end
 end
