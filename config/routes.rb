cfe8c9113438255426c03f5ee5fc523bc641b5b7
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-12-18 20:06:46 +0900

    口コミで投稿された画像を表示できる様に例外的なリダイレクト処理の対象に'active_storageファイル'を除外する様設定

--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,7 @@ Rails.application.routes.draw do
   end
   get '/search', to: 'tops#search'
   post '/:name', to: 'experiences#edit'
-  get '*path', to: redirect('/')
+  get '*path', to: redirect('/'), constraints: lambda { |req|
+    req.path.exclude? 'rails/active_storage'
+  }
 end
