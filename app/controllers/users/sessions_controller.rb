a47be7863846ff9ca78eac91a82dc6eee9ff3767
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2020-12-30 19:31:46 +0900

    add: 'Sessionsコントローラー'にゲストユーザーとしてログインできる様に、'new_guestメソッド'を定義 #116

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,6 +4,12 @@ module Users
   class SessionsController < Devise::SessionsController
     # before_action :configure_sign_in_params, only: [:create]
 
+    def new_guest
+      user = User.guest
+      sign_in user
+      redirect_root
+    end
+
     # GET /resource/sign_in
     # def new
     #   super
