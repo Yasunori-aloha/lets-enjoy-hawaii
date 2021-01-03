fd92214e278d6204b17645ad8e9183fb097da2ed
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-03 20:29:04 +0900

    update: トップページとマイページトップをパンくずリストでつなげない様にするため、'breadcrumbs.rb'の設定を修正 #119

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,9 +1,10 @@
-crumb :root do
+crumb :top_page do
   link "トップページ", root_path
 end
 
 crumb :search do |search|
   link "#{search.name}", "/#{search.search}"
+  parent :top_page
 end
 
 crumb :search_word do |search_word|
@@ -12,6 +13,7 @@ crumb :search_word do |search_word|
   else
     link "'#{search_word}' の検索結果", search_path
   end
+  parent :top_page
 end
 
 crumb :activity do |activity|
@@ -21,18 +23,26 @@ end
 
 crumb :mypage_top do |user_id|
   link 'マイページトップ', user_path(user_id)
+end
 
-crumb :mypage_edit do
-  link '会員情報の確認・変更', edit_user_registration
+crumb :mypage_edit do |user_id|
+  link '会員情報の確認・変更', edit_user_registration_path
+  parent :mypage_top, user_id
+end
 
 crumb :mypage_favorites do |user_id|
   link 'お気に入りした場所', user_favorites_path(user_id)
+  parent :mypage_top, user_id
+end
 
 crumb :mypage_reviews do |user_id|
   link '今までの口コミ', reviews_user_path(user_id)
+  parent :mypage_top, user_id
+end
 
 crumb :mypage_histories do |user_id|
   link '行った場所', user_histories_path(user_id)
+  parent :mypage_top, user_id
 end
 
 # If you want to split your breadcrumbs configuration over multiple files, you
