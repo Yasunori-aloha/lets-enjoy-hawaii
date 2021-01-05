d21192313986f339214525003734597e6958a6ad
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-05 19:24:48 +0900

    update: ユーザーモデル単体テストコードの例を日本語化するため、'user_spec.rb'を修正 #117

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,16 +12,16 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context 'ユーザーの新規登録ができる場合' do
-      it 'name・email・passwordが正しく入力されていれば登録できる' do
+      it '名前・メールアドレス・パスワードが正しく入力されていれば登録できる' do
         expect(user).to be_valid
       end
 
-      it 'introduceが入力されていなくても登録できる。' do
+      it '自己紹介が入力されていなくても登録できる。' do
         user.introduce = nil
         expect(user).to be_valid
       end
 
-      it 'adminが入力されていなくても登録できる。' do
+      it '管理者設定が入力されていなくても登録できる。' do
         user.admin = nil
         expect(user).to be_valid
       end
@@ -33,68 +33,68 @@ RSpec.describe User, type: :model do
     end
 
     context 'ユーザーの新規登録ができない場合' do
-      it 'nameが入力されていないと登録できない' do
+      it '名前が入力されていないと登録できない' do
         user.name = nil
         user.valid?
         expect(user.errors.full_messages).to include 'Nameを入力してください'
       end
 
-      it 'emailが入力されていないと登録できない' do
+      it 'メールアドレスが入力されていないと登録できない' do
         user.email = nil
         user.valid?
         expect(user.errors.full_messages).to include 'メールアドレスを入力してください'
       end
 
-      it '既に登録されているemailだと登録できない。' do
+      it '既に登録されているメールアドレスだと登録できない。' do
         user.save
         another_user.email = user.email
         another_user.valid?
         expect(another_user.errors.full_messages).to include 'メールアドレスはすでに存在します'
       end
 
-      it 'emailに"@"が入っていないと登録できない' do
+      it 'メールアドレスに"@"が入っていないと登録できない' do
         user.email = 'testtest.com'
         user.valid?
         expect(user.errors.full_messages).to include 'メールアドレスは不正な値です'
       end
 
-      it 'passwordが入力されていないと登録できない' do
+      it 'パスワードが入力されていないと登録できない' do
         user.password = nil
         user.valid?
         expect(user.errors.full_messages).to include 'パスワードを入力してください'
       end
 
-      it 'passswordが7文字以下だと登録できない' do
+      it 'pパスワードが7文字以下だと登録できない' do
         user.password = '1234567'
         user.valid?
         expect(user.errors.full_messages).to include 'パスワードは不正な値です'
       end
 
-      it 'passwordの中に数字が入っていないと登録できない' do
+      it 'パスワードの中に数字が入っていないと登録できない' do
         user.password = 'abcdef!^'
         user.valid?
         expect(user.errors.full_messages).to include 'パスワードは不正な値です'
       end
 
-      it 'passwordの中に英語(大文字・小文字問わず)が入っていないと登録できない' do
+      it 'パスワードの中に英語(大文字・小文字問わず)が入っていないと登録できない' do
         user.password = '123456!^'
         user.valid?
         expect(user.errors.full_messages).to include 'パスワードは不正な値です'
       end
 
-      it 'passwordの中に記号が2回以上入っていないと登録できない' do
+      it 'パスワードの中に記号が2回以上入っていないと登録できない' do
         user.password = '1234567a'
         user.valid?
         expect(user.errors.full_messages).to include 'パスワードは不正な値です'
       end
 
-      it 'password_confirmationが入力されていないと登録できない' do
+      it '確認用パスワードが入力されていないと登録できない' do
         user.password_confirmation = nil
         user.valid?
         expect(user.errors.full_messages).to include '確認用パスワードを入力してください'
       end
 
-      it 'password_confirmationがpasswordと同じ内容じゃないと登録できない' do
+      it '確認用パスワードがパスワードと同じ内容じゃないと登録できない' do
         user.password_confirmation = '12345678'
         user.valid?
         expect(user.errors.full_messages).to include '確認用パスワードとパスワードの入力が一致しません'
