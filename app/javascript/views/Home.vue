7032223020d755c8ec01525ff7f8a84898bd1f5f
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-23 02:20:03 +0900

    update: ホーム画面の高さ及び背景画像を表示させる様に、'Home.vue'を修正 #132

--- app/javascript/views/Home.vue
+++ app/javascript/views/Home.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class='search__wrapper'>
+    <div class="search__left"></div>
+    <div class="search__right"></div>
   </div>
 </template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+  .search__wrapper{
+    background: url('/assets/toppage.jpg');
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    height: calc(100vh - 185px);
+  }
+</style>
