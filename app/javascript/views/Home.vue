1c38bc5e670278af0b257c86681ade74a57cfdbf
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-23 22:55:19 +0900

    update: 'Home.vue'を修正して、ホーム画面にキーワード検索欄を実装 #132

--- app/javascript/views/Home.vue
+++ app/javascript/views/Home.vue
@@ -43,6 +43,25 @@
         </div>
         <div class="searh__word__wrapper">
           <SearchHeader name='キーワードから探す' />
+          <div class="search__word__main">
+            <form action="/search" accept-charset='UTF-8' method='get' class="form">
+              <div class="keyword">
+                <label for="q_name_cont" class="main__word">キーワード</label>
+                <input placeholder="ホテル名・地名やイベント名など" class="text__field"  type="search" name="q[name_cont]" id="q_name_cont">
+              </div>
+              <div class="select">
+                <label for="q_score_gteq" class="main__select">評価</label>
+                <select name="q[score_gteq]" id="score" class="select__box">
+                  <option value="0">------------</option>
+                  <option value="1">★1.0以上</option>
+                  <option value="2">★2.0以上</option>
+                  <option value="3">★3.0以上</option>
+                  <option value="4">★4.0以上</option>
+                </select>
+              </div>
+              <input type="submit" name="commit" value="検索" class="submit btn__hover">
+            </form>
+          </div>
         </div>
       </div>
       <div class="search__right">
@@ -131,7 +150,7 @@ export default {
     width: 435px;
     margin-right: 25px;
   }
-  /* 目的から探すらn */
+  /* 目的から探す欄 */
   .search__type__wrapper{
     height: 325px;
     background-color: rgba(255, 255, 255, 0.85);
@@ -173,6 +192,61 @@ export default {
     background-color: rgba(255, 255, 255, 0.85);
     backdrop-filter: blur(5px);
     margin-top: 10px;
+  }
+  .search__word__main{
+    height: 197px;
+    width: 435px;
+  }
+  .form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .keyword{
+    margin: 0 auto 10px 0;
+    padding-left: 20px;
+  }
+  .main__word{
+    margin-right: 10px;
+    color: #646464;
+    font-weight: bold;
+  }
+  .text__field{
+    margin: 10px 0;
+    padding-left: 5px;
+    height: 32px;
+    width: 300px;
+    border: 1px solid #808080;
+    border-radius: 4px;
+  }
+  .select{
+    margin-right: auto;
+    padding-left: 68px;
+  }
+  .main__select{
+    margin-right: 10px;
+    color: #646464;
+    font-weight: bold;
+  }
+  .select__box{
+    margin: 10px 0;
+    height: 37px;
+    border: 1px solid #808080;
+    border-radius: 4px;
+  }
+  .submit{
+    margin-top: 10px;
+    height: 54px;
+    width: 300px;
+    cursor: pointer;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    background-color: #90b200;
+    font-size: 18px;
+  }
+  .btn__hover:hover{
+    background-color: #ffa500;
   }
   .search__right{
     height: 576px;
