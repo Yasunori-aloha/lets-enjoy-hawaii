8e9f98a67bfefef5f29680298eb0568ca1b75ff3
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-24 02:37:50 +0900

    update: ホーム画面の地図をホバーすると画像表示が変更される様に、'HomeIslandMap.vue'と'IslandArea.vue'を設定 #132

--- app/javascript/components/HomeIslandMap.vue
+++ app/javascript/components/HomeIslandMap.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="search__map__main">
-    <img src="/images/hawaiian_islands.png" alt="ハワイ地図" class="islands__image" usemap="#ImageMap1">
+    <img :src="mapImages[mapImageIndex]" alt="ハワイ地図" class="islands__image" usemap="#ImageMap1">
     <map name="ImageMap1">
-      <IslandArea v-for="n in 8" :areaId="n - 1" />
+      <IslandArea v-for="n in 8" :areaId="n - 1" @map-change="mapImageIndex = $event" @map-return="mapImageIndex = $event" />
     </map>
   </div>
 </template>
@@ -10,6 +10,22 @@
 <script>
 import IslandArea from '../components/IslandArea.vue'
 export default {
+  data() {
+    return {
+      mapImageIndex: 8,
+      mapImages: [
+        '/images/hawaiian_islands_niihau.png',
+        '/images/hawaiian_islands_oahu.png',
+        '/images/hawaiian_islands_kahoolawe.png',
+        '/images/hawaiian_islands_lanai.png',
+        '/images/hawaiian_islands_molokai.png',
+        '/images/hawaiian_islands_maui.png',
+        '/images/hawaiian_islands_kauai.png',
+        '/images/hawaiian_islands_island_of_hawaii.png',
+        '/images/hawaiian_islands.png'
+      ],
+    }
+  },
   components: {
     IslandArea,
   },
