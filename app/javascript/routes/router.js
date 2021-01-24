05583a21c1b493856d63c17a252ea52e68033e6b
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-24 18:35:12 +0900

    add: 会員登録用の'SignInUp.vue'を作成及びルーティング設定 #132

--- app/javascript/routes/router.js
+++ app/javascript/routes/router.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home.vue'
+import SignInUp from '../views/SignInUp.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    {
-    path: '/',
-    component: Home
-  }
+    { path: '/', component: Home },
+    { path: '/users/sign_up', component: SignInUp},
   ]
 });
