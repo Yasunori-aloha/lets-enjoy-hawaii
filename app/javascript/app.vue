cea03d0293b7d1f7a2614d8f27f83bace758b27c
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-24 18:25:21 +0900

    update: 会員登録・ログイン画面でヘッダーとフッターが表示されない様に、'app.vue'を修正 #132

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <Header />
+    <Header v-if="checkSignInLogIn()" />
     <router-view />
-    <Footer />
+    <Footer v-if="checkSignInLogIn()" />
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    // 会員登録・ログインページでは、ヘッダーとフッターを表示させない様にする。
+    checkSignInLogIn() {
+      if (this.$route.path === '/users/sign_in' || this.$route.path === 'users/sign_up') {
+        return false;
+      }else{
+        return true;
+      }
+    },
+  },
+}
+</script>
 
 <style>
   .link__hover{
