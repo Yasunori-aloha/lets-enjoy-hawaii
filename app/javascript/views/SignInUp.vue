251f6b1037315a73f82ae107a7479354fe094696
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-24 19:07:44 +0900

    update: 会員登録・ログイン画面のフッターを実装 #132

--- app/javascript/views/SignInUp.vue
+++ app/javascript/views/SignInUp.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>test</h1>
+  <div class="sign__in__up__wrapper">
+    <ul class="sign__in__up__footer__wrapper">
+      <li class="sign__in__up__footer__link">
+        <a href="mailto:t26.yasunori.yamasaki@gmail.com" class="leh__email">ヘルプ・お問い合わせ</a>
+      </li>
+      <li class="sign__in__up__copyright">
+        Copyright© yasunori_yamasaki All Rights Reserved.
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -9,5 +18,29 @@ export default {
 </script>
 
 <style scoped>
-
+  .sign__in__up__wrapper{
+    background: url('/assets/register_image.jpg');
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    height: 100vh;
+  }
+  .sign__in__up__footer__wrapper{
+    height: 60px;
+    width: 100%;
+    text-align: center;
+  }
+  .sign__in__up__footer__link{
+    margin-bottom: 8px;
+  }
+  .leh__email{
+    margin-bottom: 10px;
+    color: white;
+    font-size: 10px;
+    text-decoration: none;
+  }
+  .sign__in__up__footer__wrapper{
+    color: white;
+    font-size: 10px;
+  }
 </style>
