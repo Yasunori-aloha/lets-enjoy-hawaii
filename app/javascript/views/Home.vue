4d70ddb0c37326722865da9a5fbeae591d03644f
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-24 16:39:18 +0900

    clean: キーワード検索欄をテンプレートに切り出し #132

--- app/javascript/views/Home.vue
+++ app/javascript/views/Home.vue
@@ -20,23 +20,7 @@
         <div class="searh__word__wrapper">
           <SearchHeader searchHeaderName='キーワードから探す' />
           <div class="search__word__main">
-            <form action="/search" accept-charset='UTF-8' method='get' class="form">
-              <div class="keyword">
-                <label for="q_name_cont" class="main__word">キーワード</label>
-                <input placeholder="ホテル名・地名やイベント名など" class="text__field"  type="search" name="q[name_cont]" id="q_name_cont">
-              </div>
-              <div class="select">
-                <label for="q_score_gteq" class="main__select">評価</label>
-                <select name="q[score_gteq]" id="score" class="select__box">
-                  <option value="0">------------</option>
-                  <option value="1">★1.0以上</option>
-                  <option value="2">★2.0以上</option>
-                  <option value="3">★3.0以上</option>
-                  <option value="4">★4.0以上</option>
-                </select>
-              </div>
-              <input type="submit" name="commit" value="検索" class="submit btn__hover">
-            </form>
+            <SearchForm />
           </div>
         </div>
       </div>
@@ -66,12 +50,15 @@ import SearchHeader from '../components/SearchHeader.vue';
 import HomeTypeButton from '../components/HomeTypeButton.vue';
 import HomeIslandName from '../components/HomeIslandName.vue';
 import HomeIslandMap from '../components/HomeIslandMap.vue';
+import SearchForm from '../components/SearchForm.vue';
+
 export default {
   components: {
     SearchHeader,
     HomeTypeButton,
     HomeIslandName,
     HomeIslandMap,
+    SearchForm,
   }
 };
 </script>
@@ -120,54 +107,6 @@ export default {
   .search__word__main{
     height: 197px;
     width: 435px;
-  }
-  .form{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .keyword{
-    margin: 0 auto 10px 0;
-    padding-left: 20px;
-  }
-  .main__word{
-    margin-right: 10px;
-    color: #646464;
-    font-weight: bold;
-  }
-  .text__field{
-    margin: 10px 0;
-    padding-left: 5px;
-    height: 32px;
-    width: 300px;
-    border: 1px solid #808080;
-    border-radius: 4px;
-  }
-  .select{
-    margin-right: auto;
-    padding-left: 68px;
-  }
-  .main__select{
-    margin-right: 10px;
-    color: #646464;
-    font-weight: bold;
-  }
-  .select__box{
-    margin: 10px 0;
-    height: 37px;
-    border: 1px solid #808080;
-    border-radius: 4px;
-  }
-  .submit{
-    margin-top: 10px;
-    height: 54px;
-    width: 300px;
-    cursor: pointer;
-    color: #fff;
-    border: none;
-    border-radius: 4px;
-    background-color: #90b200;
-    font-size: 18px;
   }
   .search__right{
     height: 576px;
