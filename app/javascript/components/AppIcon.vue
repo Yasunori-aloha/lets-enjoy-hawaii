2c0aacf61b7e673fc8cc36412eaa6a57d7da494e
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-24 21:30:45 +0900

    clean: アプリアイコンを再利用するため、テンプレートに切り出し #132

---
+++ app/javascript/components/AppIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-link to="/">
+    <img src="/assets/leh_logo.png" alt="アイコン画像" class="leh__icon">
+  </router-link>
+</template>
+
+<style scoped>
+.leh__icon{
+  height: 80px;
+  width: 80px;
+  margin-bottom: 5px;
+}
+</style>
--- app/javascript/components/Header.vue
+++ app/javascript/components/Header.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="leh__header">
-    <router-link to="/" class='root__to'>
-      <img src="/assets/leh_logo.png" alt="アイコン画像" class="leh__icon">
-    </router-link>
+    <AppIcon />
     <ul class="signup__login__wrapper">
       <li>
         <a href="/users/sign_in" class="sl__link link__hover">ログイン</a>
@@ -15,8 +13,12 @@
 </template>
 
 <script>
-export default {
+import AppIcon from '../components/AppIcon.vue';
 
+export default {
+  components: {
+    AppIcon,
+  }
 };
 </script>
 
@@ -27,11 +29,6 @@ export default {
     display: flex;
     justify-content: space-between;
     padding: 10px 190px 0 50px;
-  }
-  .leh__icon{
-    height: 80px;
-    width: 80px;
-    margin-bottom: 5px;
   }
   .signup__login__wrapper{
     align-items: center;
