852e47799aa86cefea0bc560aa93ce0ddab313e5
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-24 22:17:11 +0900

    update: ログイン画面か登録画面かでリンクが変更される様に、'SignInUpLink.vue'を修正 #132

--- app/javascript/components/signInUp/SignInUpLink.vue
+++ app/javascript/components/signInUp/SignInUpLink.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="login__link__wrapper">
+  <div v-if="checkSignIn()" class="login__link__wrapper">
     <span class="login__link__title">アカウントをお持ちではありませんか？</span>
     <a href="/users/sign_up" class="login__text link__hover__not__underline">会員登録</a>
+    <br>
+  </div>
+  <div v-else class="login__link__wrapper">
+    <span class="login__link__title">すでにアカウントをお持ちですか？</span>
+    <a href="/users/sign_in" class="login__text link__hover__not__underline">ログイン</a>
     <br>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    checkSignIn() {
+      if (this.$route.path === '/users/sign_in') {
+        return true;
+      }else{
+        return false;
+      }
+    },
+  },
 };
 </script>
 
@@ -29,5 +42,8 @@ export default {
   text-align: center;
   text-decoration: none;
   color: #008dde;
+}
+.link__hover__not__underline:hover {
+  color: #ffa500;
 }
 </style>
