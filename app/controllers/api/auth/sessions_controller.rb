b64ee962581f1bdb0991113709aad438056e9010
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-27 02:01:11 +0900

    update: API版の'ゲストユーザーログイン機能'を実装 #132

--- app/controllers/api/auth/sessions_controller.rb
+++ app/controllers/api/auth/sessions_controller.rb
@@ -2,6 +2,15 @@ module Overrides
   class Api::Auth::SessionsController < DeviseTokenAuth::SessionsController
 
     def new_guest
+      @resource = User.guest
+
+      # 'access-token'等を生成して、'Headers'に含んでレスポンスを返す。
+      @token = @resource.create_token
+      @resource.save
+
+      sign_in(:user, @resource, store: false, bypass: false)
+
+      render_create_success
     end
 
   end
