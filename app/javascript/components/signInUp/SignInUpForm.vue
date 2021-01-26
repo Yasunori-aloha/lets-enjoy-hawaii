308248ca9c6397c636451fef50792e71139a9d44
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-27 02:50:49 +0900

    update: ゲストユーザーログインボタンを押した際に、CSRFトークンも送信する様に設定 #132

--- app/javascript/components/signInUp/SignInUpForm.vue
+++ app/javascript/components/signInUp/SignInUpForm.vue
@@ -17,6 +17,10 @@ import SocialLoginLink from '../signInUp/SocialLoginLink.vue';
 import SignInUpLink from '../signInUp/SignInUpLink.vue';
 import SignInUpInput from '../signInUp/SignInUpInput.vue';
 import { pathCheck } from "../../pathCheck.js";
+import axios from 'axios';
+import { csrfToken } from "rails-ujs";
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken();
 
 export default {
   mixins: [pathCheck],
@@ -27,7 +31,6 @@ export default {
   },
   methods: {
     guestUserLogin() {
-      console.log('test');
     }
   },
 };
