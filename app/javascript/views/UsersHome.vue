3aa3edd0d9264d306d4dd90619d38d07961d68bf
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-29 19:39:15 +0900

    update: ユーザーページのヘッダーを実装 #132

--- app/javascript/views/UsersHome.vue
+++ app/javascript/views/UsersHome.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="my__page__image">
     <div class="my__page__wrapper">
-      <div class="my__page__head"></div>
+      <div class="my__page__head">
+        <ul class="my__page__head__list">
+          <li class="my__page__head__number">お気に入りした場所 {{ 0 }}</li>
+          <li class="my__page__head__number">口コミ数{{ 0 }}</li>
+          <li class="my__page__head__number">行った場所{{ 0 }}</li>
+        </ul>
+      </div>
       <div class="my__page__main"></div>
     </div>
   </div>
@@ -28,5 +34,23 @@ export default {
 .my__page__wrapper{
   background-color: rgba(0, 0, 0, 0.5);
   min-height: calc(100vh - 185px);
+}
+/* ユーザーページヘッダー */
+.my__page__head{
+  border-bottom: 1px solid #646464;
+  display: flex;
+  height: 94px;
+  justify-content: center;
+}
+.my__page__head__list{
+  color: white;
+  font-size: 16px;
+  height: 100%;
+  padding-top: 12px;
+  text-align: end;
+  width: 950px;
+}
+.my__page__head__number{
+  margin-bottom: 2px;
 }
 </style>
