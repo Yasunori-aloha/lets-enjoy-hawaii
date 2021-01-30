bd53512da5982db61a6eaf33b119371f88b5e1af
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-30 19:44:17 +0900

    add: ユーザーページの表示要素をルーティング及びファイル作成 #132

--- app/javascript/routes/router.js
+++ app/javascript/routes/router.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home.vue'
 import SignInUp from '../views/SignInUp.vue'
-import UsersHome from '../views/UsersHome.vue'
+import Users from '../views/Users.vue'
+import UsersMain from '../components/UsersPage/UsersMain.vue'
+import UsersFavorites from '../components/UsersPage/UsersFavorites.vue'
+import UsersReviews from '../components/UsersPage/UsersReviews.vue'
+import UsersHistories from '../components/UsersPage/UsersHistories.vue'
 
 Vue.use(Router)
 
@@ -15,6 +19,14 @@ export default new Router({
     { path: '/users/sign_up', component: SignInUp },
     { path: '/users/sign_in', component: SignInUp },
     // マイページ
-    { path: '/users/:id', component: UsersHome },
+    { path: '/users/:id', components: {
+      default: Users,
+      usersMain: UsersMain,
+    },
+    children: [
+      { path: 'favorites', component: UsersFavorites },
+      { path: 'reviews', component: UsersReviews },
+      { path: 'histories', component: UsersHistories },
+    ] },
   ]
 });
