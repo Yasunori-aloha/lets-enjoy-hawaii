1a0a1899238ec169b929e8699e67cd5714844a0c
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-31 02:16:42 +0900

    remove: Userモデルで必要のないアソシエーションを'user_serializer.rb'から削除 #132

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,7 +3,5 @@ class UserSerializer < ActiveModel::Serializer
 
   has_many :reviews, dependent: :destroy
   has_many :histories, dependent: :destroy
-  has_many :his_exps, through: :histories, source: :experience
   has_many :favorites, dependent: :destroy
-  has_many :fav_exps, through: :favorites, source: :experience
 end
