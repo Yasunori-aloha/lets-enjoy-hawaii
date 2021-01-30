3819b065ade00a4ed50a2e0b19615f49c126aba8
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-31 02:36:34 +0900

    update: お気に入りに対するコメントのparamsが変更されていたので、修正 #132

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,15 +1,18 @@
 class Api::FavoritesController < ApplicationController
-  before_action :authenticate_user!, only: %i[index create destroy update]
+  before_action :authenticate_user!, only: %i[index update]
 
   def index
-    @favorites = Favorite.preload([{ experience: :favorites }, { experience: :area }]).where(user_id: current_user.id).order('created_at DESC')
     user_is_current_user?(params)
+    @favorites = Favorite.preload([{ experience: :favorites }, { experience: :area }]).where(user_id: current_user.id).order('created_at DESC')
+
     render json: @favorites, include: ['experience']
   end
 
   def update
-    Favorite.find(params[:id]).update(comment: params[:favorite][:comment])
-    redirect_to action: :index
+    @favorite = Favorite.find(params[:id])
+    @favorite.update(comment: params[:comment])
+
+    render json: @favorite
   end
 
 end
