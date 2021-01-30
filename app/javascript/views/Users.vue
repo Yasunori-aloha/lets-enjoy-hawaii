cacb26288ffdf55b3cadee09a93f084f6319260c
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-30 20:07:16 +0900

    clean: ユーザーホーム画面の要素をコンポーネントに切り出し #132

--- app/javascript/views/Users.vue
+++ app/javascript/views/Users.vue
@@ -19,30 +19,26 @@
             </ul>
           </div>
         </div>
-        <div v-if="this.$route.path === `/users/${userData.id}`" class="my__page__main__right">
-          <div class="my__page__user__info__head">
-            <img src="/assets/no_image.jpg" alt="no_image"  class="my__page__user__image">
-            <div class="my__page__user__name__area">
-              <p class="my__page__user__name1">{{ userData.name }}</p>
-              <span class="my__page__user__name2">さんのマイページ</span>
-            </div>
-          </div>
-          <div class="my__page__user__info__main">
-            <span>{{ userData.introduce }}</span>
-          </div>
-        </div>
-        <router-view></router-view>
+        <UsersMain v-if="isUsersHome" />
+        <router-view v-else />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import UsersMain from '../components/UsersPage/UsersMain.vue';
 import { mapGetters } from "vuex";
 
 export default {
+  components: {
+    UsersMain,
+  },
   computed: {
     ...mapGetters(["userData"]),
+    isUsersHome() {
+      return this.$route.path === `/users/${this.userData.id}`;
+    },
     userReviewCounts() {
       if (this.$store.getters.userReviews) {
         return this.$store.getters.userReviews.length;
@@ -134,6 +130,9 @@ export default {
   .my__page__side__menu:hover{
     opacity: 0.7;
   }
+</style>
+
+<style>
 /* ユーザーページメインコンテンツ */
   .my__page__main__right{
     border-left: 1px solid #646464;
@@ -141,46 +140,5 @@ export default {
     height: 100%;
     width: 1004px;
     padding: 0 243px 0 28px;
-  }
-  .my__page__user__info__head{
-    min-height: 185px;
-    width: 100%;
-    background-color: rgba(255, 255, 255, 0.75);
-    display: flex;
-    margin: 28px 0;
-    border-radius: 5px;
-  }
-  .my__page__user__image{
-    height: 150px;
-    width: 150px;
-    margin: 17px;
-    border-radius: 10px;
-    border: 1px solid gray;
-  }
-  .my__page__user__name__area{
-    min-height: 150px;
-    width: calc(100% - 150px);
-    margin: 17px 17px 17px 0;
-    color: black;
-    text-align: end;
-  }
-  .my__page__user__name1{
-    min-height: calc(100% / 4 * 3);
-    line-height: 112px;
-    font-size: 60px;
-  }
-  .my__page__user__name2{
-    font-size: 22px;
-    line-height: 37px;
-  }
-  .my__page__user__info__main{
-    width: 100%;
-    background-color: rgba(255, 255, 255, 0.75);
-    margin: 20px 0 28px;
-    border-radius: 5px;
-    min-height: 250px;
-    display: block;
-    color: black;
-    padding: 25px 17px;
   }
 </style>
