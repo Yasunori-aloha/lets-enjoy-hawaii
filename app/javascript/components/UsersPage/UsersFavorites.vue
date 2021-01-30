c01a1ed275c14eb92c65b341b13639c320cf4830
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-30 20:27:44 +0900

    update: お気に入り一覧画面のHTML要素記述 #132

---
+++ app/javascript/components/UsersPage/UsersFavorites.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="my__page__main__right">
+    <div class="user__favorites__wrapper">
+      <div class="user__page__menu"></div>
+      <div class="user__favorites__show__areaul user__favorites__list">
+        <li class="favorite__wrapper">
+          <div class="favorite__info">
+            <ul class="favorite__time">
+              <li></li>
+              <li class="favorited__at"></li>
+            </ul>
+            <div class="favorite__reloease__btn">
+              <i class="release__mark"></i><span class="release__message"></span>
+              <div class="favorite__release"></div>
+            </div>
+          </div>
+          <div class="activity_picture">
+            <img src="" alt="" class="content_picture">
+            <div class="activity__picture__link"></div>
+          </div>
+          <div class="activity__info__wrapper">
+            <div class="activity__name link__hover"></div>
+            <p class="activity__place"></p>
+            <span class="favorites__counts"><i class="star__icon"></i></span>
+            <form action="">
+              <label for="" class="activity__comment__info"></label><input type="text" class="activity__comment">
+              <div class="save__btn"></div>
+            </form>
+          </div>
+        </li>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+
+</style>
