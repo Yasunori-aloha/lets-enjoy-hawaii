707c49cc24c9280f5100734db9e3acbc7b0b6206
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-01-31 20:13:54 +0900

    update: 訪問記録ページの親要素設置 #132

---
+++ app/javascript/components/UsersPage/UsersHistories.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="my__page__main__right">
+    <div class="user__histories__wrapper">
+      <UsersPageMenu currentPage="行った場所" />
+      <div class="user__histories__show__area"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UsersPageMenu from '../UsersPage/UsersPageMenu.vue';
+
+export default {
+  components: {
+    UsersPageMenu,
+  },
+};
+</script>
+
+<style scoped>
+.user__histories__wrapper{
+  height: 100%;
+  width: 776px;
+  margin: 28px 0;
+  padding: 0 17px;
+  border-radius: 5px;
+  color: black;
+  display: block;
+  background-color: rgba(255, 255, 255, 0.75);
+}
+.user__histories__show__area{
+  min-height: calc(100vh - 369px);
+  padding-top: 17px;
+}
+</style>
