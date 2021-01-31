24f87161327569359491649cfe799841dd3acbfb
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-01 00:40:10 +0900

    update: アクティビティの取得するカラムの細かな条件設定 #132

--- app/serializers/experience_serializer.rb
+++ app/serializers/experience_serializer.rb
@@ -1,14 +1,29 @@
 class ExperienceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :outline, :address, :latitude, :longitude, :business_hours_start, :business_hours_finish, :score, :heading, :pitch, :fov, :zoom, :image_url
-
-  attribute :favorite_counts, unless: :user_histories_page?
-  attribute :histories_counts, unless: :user_favorites_page?
+  attributes :id,
+             :name,
+             :latitude,
+             :longitude,
+             :heading,
+             :pitch,
+             :fov,
+             :zoom
 
   # has_many :reviews
   belongs_to :area
-  belongs_to :genre, if: :show_experiences?
-  has_many :histories, if: :show_experiences?
-  has_many :favorites, if: :show_experiences?
+
+  attribute  :favorite_counts,        unless: :user_histories_page?
+  attribute  :histories_counts,       unless: :user_favorites_page?
+
+  # アクティビティ詳細ページでのみ取得する。
+  attribute  :outline,                if: :show_experiences?
+  attribute  :address,                if: :show_experiences?
+  attribute  :business_hours_start,   if: :show_experiences?
+  attribute  :business_hours_finish,  if: :show_experiences?
+  attribute  :score,                  if: :show_experiences?
+  attribute  :image_url,              if: :show_experiences?
+  belongs_to :genre,                  if: :show_experiences?
+  has_many   :histories,              if: :show_experiences?
+  has_many   :favorites,              if: :show_experiences?
 
   def show_experiences?
     @instance_options.key?(:show_experiences)
