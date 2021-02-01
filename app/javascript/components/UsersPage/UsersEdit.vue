2e9b6f4b1598b513b849dff475872ac29b1597d9
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-01 18:25:54 +0900

    update: ユーザー情報更新エリア設定 #132

--- app/javascript/components/UsersPage/UsersEdit.vue
+++ app/javascript/components/UsersPage/UsersEdit.vue
@@ -1,5 +1,10 @@
 <template>
-
+  <div class="my__page__main__right">
+    <div class="user__edit__wrapper">
+      <p class="guest__user__alert"></p>
+      <form action="" class="edit__user"></form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +14,26 @@ export default {
 </script>
 
 <style scoped>
-
+  .user__edit__wrapper{
+    height: 100%;
+    width: 100%;
+    border-radius: 5px;
+    margin: 28px 0;
+    padding: 0 17px;
+    background-color: rgba(255, 255, 255, 0.75);
+    color: black;
+    display: block;
+  }
+/* ゲストユーザーアラート */
+  .guest__user__alert{
+    color: red;
+    margin: 0;
+    padding-top: 17px;
+    text-align: center;
+  }
+/* ユーザー情報更新欄 */
+  .edit__user{
+    padding-top: 17px;
+    text-align: end;
+  }
 </style>
