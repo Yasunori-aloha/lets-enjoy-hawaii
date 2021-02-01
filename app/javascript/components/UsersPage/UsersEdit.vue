bb608a8606f32fd6798cc233bae8fa9e862a7ede
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-01 18:38:12 +0900

    update: ユーザー名の入力欄を実装 #132

--- app/javascript/components/UsersPage/UsersEdit.vue
+++ app/javascript/components/UsersPage/UsersEdit.vue
@@ -3,7 +3,16 @@
     <div class="user__edit__wrapper">
       <UsersPageMenu currentPage="会員情報の確認・変更" />
       <p class="guest__user__alert"></p>
-      <form action="" class="edit__user"></form>
+      <form action="" class="edit__user">
+        <ul>
+          <li class="input__field">
+            <label for="user_name">お名前</label>
+            <input id="user_name" type="text" name="user[name]" autocomplete="name"  class="edit__user__form">
+          </li>
+          <li class="input__field"></li>
+          <li class="input__field"></li>
+        </ul>
+      </form>
     </div>
   </div>
 </template>
@@ -40,5 +49,16 @@ export default {
   .edit__user{
     padding-top: 17px;
     text-align: end;
+  }
+  .input__field{
+    width: 100%;
+    margin-bottom: 10px;
+  }
+  .edit__user__form{
+    resize: none;
+    width: calc(100% - 120px);
+    border: 1px solid gray;
+    border-radius: 6px;
+    padding: 5px;
   }
 </style>
