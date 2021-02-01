4c604992ab02a76b2fe8ff2567f3c8b1bb7475c3
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-01 15:23:43 +0900

    update: ユーザー口コミ一覧表示エリアの設定 #132

---
+++ app/javascript/components/UsersPage/UsersReviews.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="my__page__main__right">
+    <div class="user__reviews__wrapper">
+      <div class="user__reviews__show__area"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+  .user__reviews__wrapper{
+    height: 100%;
+    width: 776px;
+    margin: 28px 0;
+    padding: 0 17px;
+    border-radius: 5px;
+    color: black;
+    display: block;
+    background-color: rgba(255, 255, 255, 0.75);
+  }
+  .user__reviews__show__area{
+    min-height: calc(100vh - 369px);
+    padding-top: 17px;
+  }
+</style>
