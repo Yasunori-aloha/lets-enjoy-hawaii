f39b5f806dc39faf5a91a1d98fcc8c5187398c49
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-04 02:56:57 +0900

    update: 口コミ投稿ボタン表示欄の設定 #132

--- app/javascript/views/Experiences.vue
+++ app/javascript/views/Experiences.vue
@@ -33,6 +33,21 @@
               </li>
             </ul>
           </div>
+          <div class="experience__info__right">
+            <ul>
+              <li class="reviews__share">
+                <i class="far fa-comment-dots fa-flip-horizontal"></i>
+                <span>口コミ投稿</span>
+                <a href="" class="btn"></a>
+              </li>
+              <li class="reviews__share">
+                <i class="fas fa-share"></i>
+                <span>シェアする</span>
+                <a href="" class="btn"></a>
+              </li>
+            </ul>
+            <ul class="favorite__history__wrapper"></ul>
+          </div>
         </div>
       </div>
       <div class="experience__page__right"></div>
@@ -155,6 +170,35 @@ export default {
   }
   .genre__category{
     margin: -1px 6px 0 8px;
+  }
+/* 口コミやお気に入りボタン表示欄 */
+  .experience__info__right{
+    height: 140px;
+    width: 170px;
+    margin: 25px 0;
+    color: #333333;
+  }
+  .reviews__share{
+    height: 37px;
+    line-height: 37px;
+    font-size: 20px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    text-align: center;
+    margin-bottom: 5px;
+    position: relative;
+    background: linear-gradient(#ffffff, #e4e4e4);
+  }
+  .btn{
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+  }
+/* お気に入り・訪問記録登録表示欄 */
+  .favorite__history__wrapper{
+    display: flex;
   }
   .experience__page__right{
     width: calc(100% - 770px)
