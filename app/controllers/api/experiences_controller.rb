169149aaa344284c211dff80c0cdf87b1c13c801
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-04 00:48:35 +0900

    update: アクティビティのデータが取得できるように'api/experiences#show'に処理を追加 #132

--- app/controllers/api/experiences_controller.rb
+++ app/controllers/api/experiences_controller.rb
@@ -1,7 +1,9 @@
 class Api::ExperiencesController < Api::ApplicationController
 
   def show
+    @experience = Experience.find(params[:id])
 
+    render json: ExperienceSerializer.new(@experience, show_experiences?: true).to_json
   end
 
 end
