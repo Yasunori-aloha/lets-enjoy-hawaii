9d8e059717168f7e480e00808be8eb7ee3527c79
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-03 13:28:21 +0900

    update: ユーザーのお気に入り数をデータ取得時に一緒に取得できるように'user_serializer.rb'に属性を追加 #132

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name, :introduce, :admin, :image_url
 
+  attribute :favorites_counts, if: :current_user_page?
+  attribute :reviews_counts, if: :current_user_page?
+  attribute :histories_counts, if: :current_user_page?
+
   # ログイン処理時はデータを取得しない。
   has_many :reviews, unless: :login_or_signup?
   has_many :histories, unless: :login_or_signup?
@@ -8,6 +12,22 @@ class UserSerializer < ActiveModel::Serializer
 
   def login_or_signup?
     @instance_options.key?(:login_or_signup?)
+  end
+
+  def current_user_page?
+    @instance_options.key?(:current_user_page?)
+  end
+
+  def favorites_counts
+    object.favorites.length
+  end
+
+  def reviews_counts
+    object.reviews.length
+  end
+
+  def histories_counts
+    object.histories.length
   end
 
 end
