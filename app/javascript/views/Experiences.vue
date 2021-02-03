283193b8d6caa16a255e0acb137349148720228c
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-04 03:15:45 +0900

    clean: 口コミ投稿ボタン等のボタン類をコンポーネントに切り出し #132

--- app/javascript/views/Experiences.vue
+++ app/javascript/views/Experiences.vue
@@ -33,32 +33,7 @@
               </li>
             </ul>
           </div>
-          <div class="experience__info__right">
-            <ul>
-              <li class="reviews__share">
-                <i class="far fa-comment-dots fa-flip-horizontal"></i>
-                <span>口コミ投稿</span>
-                <a href="" class="btn"></a>
-              </li>
-              <li class="reviews__share">
-                <i class="fas fa-share"></i>
-                <span>シェアする</span>
-                <a href="" class="btn"></a>
-              </li>
-            </ul>
-            <ul class="favorite__history__wrapper">
-              <li class="favorite">
-                <i class="far fa-star"></i>
-                <a href="" class="btn"></a>
-                <span>お気に入り数{{  }}</span>
-              </li>
-              <li class="history__place">
-                <i class="fas fa-shoe-prints fa-rotate-270"></i>
-                <a href="" class="btn"></a>
-                <span class="history">行った</span>
-              </li>
-            </ul>
-          </div>
+          <ExperiencesMultiButton />
         </div>
       </div>
       <div class="experience__page__right"></div>
@@ -69,9 +44,11 @@
 
 <script>
 import ExperiencesPageMenu from '../components/ExperiencesPage/ExperiencesPageMenu.vue';
+import ExperiencesMultiButton from '../components/ExperiencesPage/ExperiencesMultiButton.vue';
 export default {
   components: {
     ExperiencesPageMenu,
+    ExperiencesMultiButton,
   }
 };
 </script>
@@ -182,67 +159,7 @@ export default {
   .genre__category{
     margin: -1px 6px 0 8px;
   }
-/* 口コミやお気に入りボタン表示欄 */
-  .experience__info__right{
-    height: 140px;
-    width: 170px;
-    margin: 25px 0;
-    color: #333333;
-  }
-  .reviews__share{
-    height: 37px;
-    line-height: 37px;
-    font-size: 20px;
-    border: 1px solid #ccc;
-    border-radius: 3px;
-    text-align: center;
-    margin-bottom: 5px;
-    position: relative;
-    background: linear-gradient(#ffffff, #e4e4e4);
-  }
-  .btn{
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-  }
-/* お気に入り・訪問記録登録表示欄 */
-  .favorite__history__wrapper{
-    display: flex;
-  }
-/* お気に入り登録ボタン */
-  .favorite{
-    height: 25px;
-    width: calc((170px - 5px) / 2) ;
-    line-height: 25px;
-    font-size: 15px;
-    border: 1px solid #ccc;
-    border-radius: 3px;
-    padding-left: 5px;
-    position: relative;
-    background: linear-gradient(#ffffff, #e4e4e4);
-  }
-  .fa-star{
-    color: #ffa500;
-  }
-/* 訪問記録登録ボタン */
-  .history__place{
-    height: 25px;
-    width: calc((170px - 5px) / 2) ;
-    line-height: 25px;
-    font-size: 15px;
-    border: 1px solid #ccc;
-    border-radius: 3px;
-    padding-left: 5px;
-    position: relative;
-    background: linear-gradient(#ffffff, #e4e4e4);
-    margin-left: 5px;
-  }
-  .history{
-    float: right;
-    margin-right: 6px;
-  }
+
   .experience__page__right{
     width: calc(100% - 770px)
   }
