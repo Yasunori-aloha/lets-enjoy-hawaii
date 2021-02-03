e6e175319958bb1c691347ae5357a09410313a0f
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-04 01:44:02 +0900

    update: ホーム画面からの遷移歴の表示を実装 #132

---
+++ app/javascript/views/Experiences.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <router-view name="header" />
+    <div class="experience__page__menu">
+      <div>
+        <router-link to="/" class="link__hover">トップページ</router-link>
+        <span> › </span>
+        <a href="/" class="link__hover">カテゴリー名{{  }}</a>
+        <span> › アクティビティ名称{{  }}</span>
+      </div>
+    </div>
+    <router-view name="footer" />
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+  .experience__page__menu{
+    width: 950px;
+    font-size: 10px;
+    margin: 0 auto;
+    padding: 10px 0 5px 0;
+    border-bottom: 1px dotted black;
+    text-align: left;
+  }
+</style>
