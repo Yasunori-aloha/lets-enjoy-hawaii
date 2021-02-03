cab1a344a7a82e996df1f17b93e1fb6c2a2fe62c
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-04 01:55:58 +0900

    update: アクティビティ詳細表示欄の親要素を設定 #132

--- app/javascript/views/Experiences.vue
+++ app/javascript/views/Experiences.vue
@@ -2,6 +2,10 @@
   <div>
     <router-view name="header" />
     <ExperiencesPageMenu />
+    <div class="experience__page__wrapper">
+      <div class="experience__page__left"></div>
+      <div class="experience__page__right"></div>
+    </div>
     <router-view name="footer" />
   </div>
 </template>
@@ -16,5 +20,19 @@ export default {
 </script>
 
 <style scoped>
-
+/* アクティビティ詳細表示欄 */
+  .experience__page__wrapper{
+  font-size: 10px;
+  margin: 0 auto;
+  width: 950px;
+  display: flex;
+  margin-bottom: 30px;
+  }
+  .experience__page__left{
+    width: 740px;
+    margin-right: 30px;
+  }
+  .experience__page__right{
+    width: calc(100% - 770px)
+  }
 </style>
