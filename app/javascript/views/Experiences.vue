d027ecb65eb654078503394ad864248961f8f3ab
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-04 02:04:22 +0900

    update: アクティビティの名称表示欄の設定 #132

--- app/javascript/views/Experiences.vue
+++ app/javascript/views/Experiences.vue
@@ -3,7 +3,18 @@
     <router-view name="header" />
     <ExperiencesPageMenu />
     <div class="experience__page__wrapper">
-      <div class="experience__page__left"></div>
+      <div class="experience__page__left">
+        <div class="experience__info__wrapper">
+          <div class="experience__info__left">
+            <ul>
+              <li class="experience__name__wrapper">
+                <img src="" class="category__icon">
+                <h1 class="experinece__name">アクティビティ名称{{  }}</h1>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <div class="experience__page__right"></div>
     </div>
     <router-view name="footer" />
@@ -31,6 +42,33 @@ export default {
   .experience__page__left{
     width: 740px;
     margin-right: 30px;
+  }
+/* アクティビティ情報表示欄 */
+  .experience__info__wrapper{
+    display: flex;
+    justify-content: space-between;
+  }
+  .experience__info__left{
+    width: 500px;
+    margin: 25px 0;
+    color: #333333;
+  }
+/* アクティビティ名称表示欄 */
+  .experience__name__wrapper{
+    display: flex;
+  }
+  .category__icon{
+    height: 35px;
+    width: 35px;
+    /* background-image: url(''); */
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+  }
+  .experinece__name{
+    font-size: 24px;
+    font-weight: bold;
+    margin: -1px 0 0 3px;
   }
   .experience__page__right{
     width: calc(100% - 770px)
