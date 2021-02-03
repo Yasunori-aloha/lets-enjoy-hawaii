fbb4065044816e941f6aaf9c021dc3364c32567c
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-03 23:38:54 +0900

    update: '訪問記録一覧ページ'の'登録解除ボタン'をクリックすると訪問記録登録解除と要素削除がされるように処理を追加 #132

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -2,7 +2,7 @@ class Api::FavoritesController < Api::ApplicationController
   before_action :authenticate_api_user!, only: %i[update destroy]
   before_action -> { current_user?(params) }, only: %i[update destroy]
 
-  # お気に入り記録へのコメント保存
+  # お気に入り記録へのコメント保存。
   def update
     @favorite = Favorite.find(params[:id])
     @favorite.update(comment: params[:favorite][:comment])
@@ -10,6 +10,7 @@ class Api::FavoritesController < Api::ApplicationController
     render json: @favorite, status: :ok
   end
 
+  # お気に入り登録解除。
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
--- app/controllers/api/histories_controller.rb
+++ app/controllers/api/histories_controller.rb
@@ -1,12 +1,21 @@
 class Api::HistoriesController < Api::ApplicationController
-  before_action :authenticate_api_user!, only: :update
+  before_action :authenticate_api_user!, only: %i[update destroy]
+  before_action -> { current_user?(params) }, only: %i[update destroy]
 
-  # 訪問記録へのコメント保存
+  # 訪問記録へのコメント保存。
   def update
     @history = History.find(params[:id])
     @history.update(comment: params[:history][:comment])
 
-    render json: @history
+    render json: @history, status: :ok
+  end
+
+  # 訪問記録登録解除。
+  def destroy
+    @history = History.find(params[:id])
+    @history.destroy
+
+    render json: true, status: :ok
   end
 
 end
