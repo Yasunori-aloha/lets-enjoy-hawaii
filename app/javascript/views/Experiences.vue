bb98499c97922c8cfdff0cf9e048abac1ed1165b
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-04 16:31:41 +0900

    clean: アクティビティ詳細ページ内表示切り替えボタン表示欄をコンポーネントに切り出し #132

--- app/javascript/views/Experiences.vue
+++ app/javascript/views/Experiences.vue
@@ -9,23 +9,7 @@
           <ExperiencesMultiButton />
         </div>
         <div>
-          <ul class="experience__tabs__wrapper">
-            <li class="tab active__tab">
-              <span>概要</span>
-            </li>
-            <li class="two__line__tab">
-              <span>口コミ</span>
-              <br>
-              <span class="number">({{  }}件)</span>
-              <a href="" class="experience__link" />
-            </li>
-            <li class="two__line__tab">
-              <span>写真</span>
-              <br>
-              <span class="number">({{  }}枚)</span>
-              <a href="" class="experience__link" />
-            </li>
-          </ul>
+          <ExperiencesTab />
         </div>
       </div>
       <div class="experience__page__right"></div>
@@ -38,11 +22,14 @@
 import ExperiencesPageMenu from '../components/ExperiencesPage/ExperiencesPageMenu.vue';
 import ExperiencesInfo from '../components/ExperiencesPage/ExperiencesInfo.vue';
 import ExperiencesMultiButton from '../components/ExperiencesPage/ExperiencesMultiButton.vue';
+import ExperiencesTab from '../components/ExperiencesPage/ExperiencesTab.vue';
+
 export default {
   components: {
     ExperiencesPageMenu,
     ExperiencesInfo,
     ExperiencesMultiButton,
+    ExperiencesTab,
   }
 };
 </script>
@@ -64,61 +51,6 @@ export default {
   .experience__info__wrapper{
     display: flex;
     justify-content: space-between;
-  }
-/* アクティビティ表示部分切り替えタブ表示欄 */
-  .experience__tabs__wrapper{
-    display: flex;
-    height: 43px;
-    border-bottom: 2px solid #ffa500;
-    padding: 0 12px;
-  }
-  .tab{
-    height: 43px;
-    width: 86px;
-    line-height: 15px;
-    border: 2px solid #ccc;
-    border-bottom: 2px solid #ffa500;
-    border-radius: 4px 4px 0 0;
-    margin-right: 4px;
-    padding: 12px 0 0;
-    font-size: 12px;
-    color: #333333;
-    text-align: center;
-    cursor: pointer;
-    position: relative;
-  }
-  .two__line__tab{
-    height: 43px;
-    width: 86px;
-    line-height: 15px;
-    border: 2px solid #ccc;
-    border-bottom: 2px solid #ffa500;
-    border-radius: 4px 4px 0 0;
-    margin-right: 4px;
-    padding: 6px 0 0;
-    font-size: 12px;
-    color: #333333;
-    text-align: center;
-    cursor: pointer;
-    position: relative;
-  }
-  .number{
-    font-size: 11px;
-  }
-  .experience__link{
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-  }
-/* 選択されているタブ表示欄 */
-  .active__tab{
-    border: 2px solid #ffa500;
-    border-bottom: 2px solid #ffffff;
-    color: orangered;
-    font-weight: bold;
-    cursor: auto;
   }
   .experience__page__right{
     width: calc(100% - 770px)
