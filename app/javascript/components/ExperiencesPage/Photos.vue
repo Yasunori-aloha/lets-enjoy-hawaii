775a3af23cb95cc59f312c05773fa0bedf973a7f
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-05 17:14:01 +0900

    update: アクティビティの写真一覧表示要素設定 #132

---
+++ app/javascript/components/ExperiencesPage/Photos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="photo__wrapper"></div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+  .photo__wrapper{
+    height: 302px;
+    height: auto !important;
+    min-height: calc(100vh - 495px);
+    margin-top: 16px;
+  }
+</style>
