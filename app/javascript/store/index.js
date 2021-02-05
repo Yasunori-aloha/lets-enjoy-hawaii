10a35624dd9bf998eb6e6d430d1bf143b4b331fa
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-05 16:43:49 +0900

    clean: 'Vuex'内の'お気に入り関連の処理'等をモジュールに切り出し #132

--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
-import { user, experience } from "./modules";
+import { user, experience, favorite } from "./modules";
 
 Vue.use(Vuex);
 
@@ -22,36 +22,6 @@ export default new Vuex.Store({
     },
   },
   actions: {
-    updateFavoriteComment: async function({}, params) {
-      await axios.patch(`/api/v1/users/${params.userId}/favorites/${params.favoriteId}`, params.formData,
-      {
-        headers: {
-          'content-type': 'multipart/form-data',
-          'access-token': localStorage.getItem('access-token'),
-          'client': localStorage.getItem('client'),
-          'uid': localStorage.getItem('uid'),
-        }
-      })
-      .catch(error => {
-        console.log(error.response.data);
-      });
-    },
-    favoriteRemove: async function({}, params) {
-      await axios.delete(`/api/v1/users/${params.userId}/favorites/${params.experienceId}`,
-      {
-        headers: {
-          'access-token': localStorage.getItem('access-token'),
-          'client': localStorage.getItem('client'),
-          'uid': localStorage.getItem('uid'),
-        }
-      })
-      .then(response => {
-        if (this.state.experience.experienceData !== null) {
-          this.state.experience.experienceData.already_favorited = false;
-          this.state.experience.experienceData.favorite_counts -= 1;
-        }
-      });
-    },
     updateHistoryComment: async function({}, params) {
       await axios.patch(`/api/v1/users/${params.userId}/histories/${params.historyId}`, params.formData,
       {
@@ -95,24 +65,10 @@ export default new Vuex.Store({
         this.state.experience.experienceData.already_historied = true;
       });
     },
-    favoriteRegistration: async function({}, params) {
-      await axios.post(`/api/v1/experiences/${params.experienceId}/favorites`,
-      {
-        headers: {
-          'access-token': localStorage.getItem('access-token'),
-          'client': localStorage.getItem('client'),
-          'uid': localStorage.getItem('uid'),
-        },
-        user_id: params.userId
-      })
-      .then(response => {
-        this.state.experience.experienceData.already_favorited = true;
-        this.state.experience.experienceData.favorite_counts += 1;
-      });
-    },
   },
   modules: {
     user,
     experience,
+    favorite,
   },
 });
--- app/javascript/store/modules/index.js
+++ app/javascript/store/modules/index.js
@@ -1,7 +1,9 @@
 import user from './user.js';
 import experience from './experience.js';
+import favorite from './favorite.js';
 
 export {
   user,
   experience,
+  favorite,
 };
