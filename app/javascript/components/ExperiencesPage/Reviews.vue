10297082a31ff9637eb0ab8877fb4dd4aa07bee4
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-05 19:28:55 +0900

    update: 口コミ件数の表示実装 #132

--- app/javascript/components/ExperiencesPage/Reviews.vue
+++ app/javascript/components/ExperiencesPage/Reviews.vue
@@ -4,6 +4,12 @@
       <div class="review__title__icon"></div>
       <h1 class="review__title">{{ experienceData.name }}の口コミ一覧</h1>
     </div>
+    <div class="review__info">
+      <div class="review__number">
+        <span>1 - {{ experienceData.reviews_counts }}件目</span>
+        <span class="review__number__all">(全{{ experienceData.reviews_counts }}件中)</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,5 +47,23 @@ export default {
     font-weight: bold;
     color: #333333;
     margin: 0 0 13px 3px;
+  }
+/* 口コミ件数表示欄 */
+  .review__info{
+    font-size: 16px;
+    padding-bottom: 6px;
+    margin-bottom: 10px;
+    border-bottom: 3px solid #eee;
+    word-spacing: 0;
+    color:  #333333;
+    display: flex;
+    justify-content: space-between;
+  }
+  .review__number{
+    display: flex;
+  }
+  .review__number__all{
+    font-size: 12px;
+    margin: 4px 0 0 5px;
   }
 </style>
