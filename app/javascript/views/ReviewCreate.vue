3e29a0f80c2211a960d8d2d184168f33866028e7
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-06 17:39:06 +0900

    update: 口コミ投稿ページのタイトル表示を実装 #132

--- app/javascript/views/ReviewCreate.vue
+++ app/javascript/views/ReviewCreate.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="review__wrapper">
     <div class="review__form">
-      <form></form>
+      <form>
+        <div class="review__form__tile">
+          <div class="review__from__title__wrapper">
+            <div class="head__icon"></div>
+            <h1 class="head__name">口コミを投稿する</h1>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -22,13 +29,31 @@ export default {
 <style scoped>
 /* 口コミ投稿ページ表示欄 */
   .review__wrapper{
-  width: 950px;
-  margin: 0 auto 100px;
-  display: flex;
-  justify-content: space-between;
+    width: 950px;
+    margin: 0 auto 100px;
+    display: flex;
+    justify-content: space-between;
   }
   .review__form{
     width: 750px;
     color: #333333;
+  }
+/* 口コミ投稿ページタイトル表示欄 */
+  .review__form__tile{
+    margin-top: 28px;
+  }
+  .review__from__title__wrapper{
+    display: flex;
+  }
+  .head__icon{
+    height: 22px;
+    width: 5px;
+    border-radius: 2px;
+    background-color: #ff4500;
+  }
+  .head__name{
+    font-size: 18px;
+    font-weight: bold;
+    margin-left: 8px;
   }
 </style>
