cd9a1b15a6b3a0c8f871865d393011dfd6163ffd
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-06 21:14:28 +0900

    clean: 口コミ評価点選択欄をコンポーネントに切り出し #132

---
+++ app/javascript/components/ReviewPage/ReviewScore.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="review__form__score">
+    <div class="review__score__title">
+      <h2 class="score__title">総合評価</h2>
+      <span class="score__info">（星をクリックして評価してください）</span>
+      <span class="required__item">必須</span>
+    </div>
+    <ul class="review__score">
+      <li v-for="index in 5">
+        <label :for="`review_score${index}`" class="review__score__star">☆</label>
+        <input type="radio" :id="`review_score${index}`" name="review[score]" :value="`${index}`" class="review__btn">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+/* 口コミの評価点選択欄 */
+  .review__form__score{
+    margin-top: 25px;
+  }
+  .review__score__title{
+    display: flex;
+    align-items: flex-end;
+    color: #ff4500;
+    border-bottom: 3px solid #ff4500;
+  }
+  .score__info{
+    font-size: 12px;
+    margin-bottom: 3px;
+  }
+/* 評価点用星表示欄 */
+  .review__score{
+    display: flex;
+    font-size: 30px;
+    margin: 5px 0 0 1px;
+  }
+  .review__score > li{
+    height: 45px;
+    width: 42px;
+    position: relative;
+    color: #ffa500;
+    display: block;
+  }
+  .review__score__star{
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+  }
+  .review__btn{
+    display: none;
+  }
+</style>
--- app/javascript/views/ReviewCreate.vue
+++ app/javascript/views/ReviewCreate.vue
@@ -23,19 +23,7 @@
             </ul>
           </div>
         </div>
-        <div class="review__form__score">
-          <div class="review__score__title">
-            <h2 class="score__title">総合評価</h2>
-            <span class="score__info">（星をクリックして評価してください）</span>
-            <span class="required__item">必須</span>
-          </div>
-          <ul class="review__score">
-            <li v-for="index in 5">
-              <label :for="`review_score${index}`" class="review__score__star">☆</label>
-              <input type="radio" :id="`review_score${index}`" name="review[score]" :value="`${index}`" class="review__btn">
-            </li>
-          </ul>
-        </div>
+        <ReviewScore />
         <ReviewText />
       </form>
     </div>
@@ -45,12 +33,14 @@
 <script>
 import ActivityPicture from '../components/ActivityPicture.vue';
 import CategoryIcon from '../components/CategoryIcon.vue';
+import ReviewScore from '../components/ReviewPage/ReviewScore.vue';
 import ReviewText from '../components/ReviewPage/ReviewText.vue';
 
 export default {
   components: {
     ActivityPicture,
     CategoryIcon,
+    ReviewScore,
     ReviewText,
   },
   computed: {
@@ -122,44 +112,6 @@ export default {
     margin-top: 5px;
     font-size: 12px;
     word-spacing: 0;
-  }
-/* 口コミの評価点選択欄 */
-  .review__form__score{
-    margin-top: 25px;
-  }
-  .review__score__title{
-    display: flex;
-    align-items: flex-end;
-    color: #ff4500;
-    border-bottom: 3px solid #ff4500;
-  }
-  .score__info{
-    font-size: 12px;
-    margin-bottom: 3px;
-  }
-/* 評価点用星表示欄 */
-  .review__score{
-    display: flex;
-    font-size: 30px;
-    margin: 5px 0 0 1px;
-  }
-  .review__score > li{
-    height: 45px;
-    width: 42px;
-    position: relative;
-    color: #ffa500;
-    display: block;
-  }
-  .review__score__star{
-    cursor: pointer;
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-  }
-  .review__btn{
-    display: none;
   }
 </style>
 
