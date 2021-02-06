3d2901871523facb9abd6b15e87840802693604f
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-06 16:36:55 +0900

    update: 口コミ投稿成功時のレスポンスの出力を実装 #132

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -2,11 +2,17 @@ class Api::ReviewsController < Api::ApplicationController
   before_action :authenticate_api_user!, only: :create
 
   def create
-    experience = Experience.find(params[:id])
+    experience = Experience.preload(
+      { reviews: { user: {image_attachment: :blob }}},
+      { reviews: :images_attachments}
+    ).find(params[:id])
     @review = Review.new(review_params)
 
     if @review.valid?
-      binding.pry
+      @review.save
+      experience.update(score: experience.reviews.average(:score).round(1))
+
+      render json: ExperienceSerializer.new(experience, show_experiences?: true, login_or_signup_or_experience?: true, current_user_id: current_api_user.id).to_json
     else
       render json: { success: false, errors: [@review.errors.full_messages]}, status: 400
     end
