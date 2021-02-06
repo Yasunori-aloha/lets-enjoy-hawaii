a485e456975cab14723cd1cd9c000c1b6dd2d31b
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-06 15:54:10 +0900

    update: 口コミ投稿時のストロングパラメーターを設定 #132

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -5,4 +5,10 @@ class Api::ReviewsController < Api::ApplicationController
     binding.pry
   end
 
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :comment, :score, images: []).merge(user_id: current_api_user.id, experience_id: params[:id])
+  end
+
 end
