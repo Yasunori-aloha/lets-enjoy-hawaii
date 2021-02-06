e68ab28696e197459d0e68451f9b63fa2c9d0110
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-06 16:16:39 +0900

    update: 口コミ投稿時にエラーが発生した場合のレスポンスの出力を実装 #132

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -2,7 +2,14 @@ class Api::ReviewsController < Api::ApplicationController
   before_action :authenticate_api_user!, only: :create
 
   def create
-    binding.pry
+    experience = Experience.find(params[:id])
+    @review = Review.new(review_params)
+
+    if @review.valid?
+      binding.pry
+    else
+      render json: { success: false, errors: [@review.errors.full_messages]}, status: 400
+    end
   end
 
   private
