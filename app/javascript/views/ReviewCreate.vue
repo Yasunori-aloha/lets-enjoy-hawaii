470123c3b210634577435b7846cedb0d395c7c2c
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-06 17:28:45 +0900

    update: 口コミ投稿ページ親要素を設定 #132

--- app/javascript/views/ReviewCreate.vue
+++ app/javascript/views/ReviewCreate.vue
@@ -1,5 +1,9 @@
 <template>
-
+  <div class="review__wrapper">
+    <div class="review__form">
+      <form></form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -16,5 +20,15 @@ export default {
 </script>
 
 <style scoped>
-
+/* 口コミ投稿ページ表示欄 */
+  .review__wrapper{
+  width: 950px;
+  margin: 0 auto 100px;
+  display: flex;
+  justify-content: space-between;
+  }
+  .review__form{
+    width: 750px;
+    color: #333333;
+  }
 </style>
