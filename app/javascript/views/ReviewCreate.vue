fdeea6df656ae95b1308d5074049e0a274ce6a82
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-06 17:58:34 +0900

    update: 口コミ投稿ページのアクティビティ情報表示エリアを設定 #132

--- app/javascript/views/ReviewCreate.vue
+++ app/javascript/views/ReviewCreate.vue
@@ -3,9 +3,24 @@
     <div class="review__form">
       <form>
         <div class="review__form__tile">
-          <div class="review__from__title__wrapper">
+          <div class="review__form__title__wrapper">
             <div class="head__icon"></div>
             <h1 class="head__name">口コミを投稿する</h1>
+          </div>
+          <div class="review__experience__info">
+            <img src="" alt="" class="experience__picture">
+            <ul class="experience__name__wrapper">
+              <li class="experience__icon__name">
+                <img src="" alt="" class="category__icon">
+                <span class="experience__name">{{ experienceData.name }}</span>
+              </li>
+              <li class="experience__area">
+                <span>エリア：{{ experienceData.area.island.name }} > {{ experienceData.area.name }}</span>
+              </li>
+              <li class="experience__address">
+                <span>住所：{{ experienceData.address }}</span>
+              </li>
+            </ul>
           </div>
         </div>
       </form>
@@ -42,7 +57,7 @@ export default {
   .review__form__tile{
     margin-top: 28px;
   }
-  .review__from__title__wrapper{
+  .review__form__title__wrapper{
     display: flex;
   }
   .head__icon{
@@ -55,5 +70,37 @@ export default {
     font-size: 18px;
     font-weight: bold;
     margin-left: 8px;
+  }
+/* アクティビティ情報表示欄 */
+  .review__experience__info{
+    display: flex;
+    margin-top: 27px;
+  }
+  .experience__picture{
+    height: 107px;
+    width: 160px;
+  }
+  .experience__name__wrapper{
+    margin-left: 15px;
+  }
+  .experience__icon__name{
+    display: flex;
+  }
+  .category__icon{
+
+  }
+  .experience__name{
+    font-weight: bold;
+    margin: 5px 0 0 8px;
+  }
+  .experience__area{
+    margin-top: 9px;
+    font-size: 11px;
+    word-spacing: 0;
+  }
+  .experience__address{
+    margin-top: 5px;
+    font-size: 12px;
+    word-spacing: 0;
   }
 </style>
