56a5fac9c21a0336b03ca3797c9e84041621d095
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-07 13:01:29 +0900

    fix: コメント等が空白でもレコード保存できてしまっていたので、'reviewData'の初期値を修正 #132

--- app/javascript/store/modules/review.js
+++ app/javascript/store/modules/review.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 export default {
   state: {
     reviewData: {
-      score: null,
-      title: null,
-      comment: null,
+      score: '',
+      title: '',
+      comment: '',
       images: [],
     },
   },
