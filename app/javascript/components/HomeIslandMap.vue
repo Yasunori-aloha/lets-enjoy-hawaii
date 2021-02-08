54eb6d5480e2e290abbe8b6d8b027934ddf40741
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-09 01:26:25 +0900

    update: '地図検索'でアクティビティデータ取得処理に行く様に処理を追加 #132

--- app/javascript/components/HomeIslandMap.vue
+++ app/javascript/components/HomeIslandMap.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="search__map__main search_map_main">
-    <img :src="mapImages[mapImageIndex]" alt="ハワイ地図" class="islands__image" usemap="#ImageMap1">
+  <div class="search__map__main">
+    <img :src="mapImageData.mapImages[mapImageData.mapImageIndex]" alt="ハワイ地図" class="islands__image" usemap="#ImageMap1">
     <map name="ImageMap1">
       <IslandArea v-for="(n, index) in 8" :areaId="index" />
     </map>
@@ -8,30 +8,16 @@
 </template>
 
 <script>
-import IslandArea from '../components/IslandArea.vue'
+import IslandArea from '../components/IslandArea.vue';
+
 export default {
-  data() {
-    return {
-      mapImages: [
-        '/images/hawaiian_islands_niihau.png',
-        '/images/hawaiian_islands_oahu.png',
-        '/images/hawaiian_islands_kahoolawe.png',
-        '/images/hawaiian_islands_lanai.png',
-        '/images/hawaiian_islands_molokai.png',
-        '/images/hawaiian_islands_maui.png',
-        '/images/hawaiian_islands_kauai.png',
-        '/images/hawaiian_islands_island_of_hawaii.png',
-        '/images/hawaiian_islands.png'
-      ],
-    }
-  },
-  computed: {
-    mapImageIndex() {
-      return this.$store.getters.mapImageIndex;
-    },
-  },
   components: {
     IslandArea,
+  },
+  computed: {
+    mapImageData() {
+      return this.$store.getters.mapImageData;
+    },
   },
 };
 </script>
