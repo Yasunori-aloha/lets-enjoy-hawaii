c352949d543c8e040281c15620cc8c876ba7000f
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-10 19:08:18 +0900

    fix: アクティビティ詳細ページ遷移時に'access-token'を送信しておらず、'お気に入り済み'かどうかの判定ができていなかったため修正 #132

--- app/javascript/store/modules/experience.js
+++ app/javascript/store/modules/experience.js
@@ -9,7 +9,14 @@ export default {
   },
   actions: {
     toExperiencesPage: async function({}, experienceId) {
-      await axios.get(`/api/v1/experiences/${experienceId}`)
+      await axios.get(`/api/v1/experiences/${experienceId}`,
+      {
+        headers: {
+          'access-token': localStorage.getItem('access-token'),
+          'client': localStorage.getItem('client'),
+          'uid': localStorage.getItem('uid'),
+        }
+      })
       .then(response => {
         this.state.experience.experienceData = response.data;
       });
