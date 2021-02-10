2a9dbaf7ba97eb5bfdd562c4ef124deaae35f444
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-10 14:12:39 +0900

    update: アクティビティ詳細ページ遷移時のみ'Islandモデル内searchカラム'のデータを取得する様に処理を追加 #132

--- app/serializers/island_serializer.rb
+++ app/serializers/island_serializer.rb
@@ -1,8 +1,20 @@
 class IslandSerializer < ActiveModel::Serializer
   attributes :name
 
+  attribute  :search, if: :show_experiences?
+
+
+  # 属性取得判定用メソッド
+  def show_experiences?
+    @instance_options.key?(:show_experiences?)
+  end
+
   # 属性取得用メソッド
   def name
     object[:name]
+  end
+
+  def search
+    object[:search]
   end
 end
