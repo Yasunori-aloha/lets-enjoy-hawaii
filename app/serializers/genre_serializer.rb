448b0b9c73d9d19b2ae81e71c185c66e9bed6b98
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-10 14:35:19 +0900

    update: アクティビティ詳細ページ遷移時のみ'Genreモデル内searchカラム'のデータを取得する様に処理を追加 #132

--- app/serializers/genre_serializer.rb
+++ app/serializers/genre_serializer.rb
@@ -1,7 +1,14 @@
 class GenreSerializer < ActiveModel::Serializer
   attributes :name
 
+  attribute  :search, if: :show_experiences?
+
   belongs_to :category, unless: :search?
+
+  # 属性取得判定用メソッド
+  def show_experiences?
+    @instance_options.key?(:show_experiences?)
+  end
 
   # 属性取得判定用メソッド
   def search?
