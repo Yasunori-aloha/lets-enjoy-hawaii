efe8170e42b203c91674ff29cd35611ae56fdbfb
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-02-12 18:55:23 +0900

    fix: ユーザー情報更新時に'imageデータ'が'null'だとバリデーションエラーが発生していたため、'imageデータ'あるかないかで処理を変える様に処理を修正 #132

--- app/controllers/api/auth/registrations_controller.rb
+++ app/controllers/api/auth/registrations_controller.rb
@@ -2,6 +2,28 @@
 module Overrides
   class Api::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
 
+    def update
+      if @resource
+        if params[:image] == 'null'
+          if @resource.send(resource_update_method, account_update_params_not_image)
+            yield @resource if block_given?
+            render_update_success
+          else
+            render_update_error
+          end
+        else
+          if @resource.send(resource_update_method, account_update_params)
+            yield @resource if block_given?
+            render_update_success
+          else
+            render_update_error
+          end
+        end
+      else
+        render_update_error_user_not_found
+      end
+    end
+
     private
 
     def sign_up_params
@@ -10,6 +32,11 @@ module Overrides
 
     def account_update_params
       params.permit(:name, :email, :introduce, :image)
+    end
+
+    # アイコン画像が添付されていなかった場合はこっちのメソッドを使用する。(エラーが発生するため。)
+    def account_update_params_not_image
+      params.permit(:name, :email, :introduce)
     end
 
     protected
