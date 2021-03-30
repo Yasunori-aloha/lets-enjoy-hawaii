0a7a0005e300b6a101f215413bf764ef92ecccee
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-03-30 19:35:12 +0900

    update: アクティビティの基本情報表示要素のレスポンシブ対応 #157

--- app/javascript/components/ExperiencesPage/TimeLocation.vue
+++ app/javascript/components/ExperiencesPage/TimeLocation.vue
@@ -19,21 +19,21 @@
     </ul>
   </div>
   <div v-else>
-    <!-- <ul class="experience__business__hours">
-      <li class="business__hours__info">
-        <span>営業時間</span>
-      </li>
-      <li class="business__hours__main">
-        <span v-if="isExists">{{ experienceData.business_hours_start }} 〜 {{ experienceData.business_hours_finish }}</span>
-      </li>
-    </ul> -->
-    <ul class="experience__location__sm">
-      <li class="location__info__sm">
+    <ul class="experience__info__sm">
+      <li class="experience__info__left__sm">
         <span>所在地</span>
       </li>
-      <li class="locaiton__main">
-        <span class="location__postal__code">{{ experienceData.address }}</span>
-        <span class="location__address">{{  }}</span>
+      <li class="experience__info__right__sm">
+        <span class="location__postal__code__sm"></span>
+        <!-- <span class="location__postal__code__sm">{{ experienceData.address }}</span> -->
+      </li>
+    </ul>
+    <ul class="experience__info__sm">
+      <li class="experience__info__left__sm">
+        <span>営業時間</span>
+      </li>
+      <li class="experience__info__right__sm">
+        <span v-if="isExists">{{ experienceData.business_hours_start }} 〜 {{ experienceData.business_hours_finish }}</span>
       </li>
     </ul>
   </div>
@@ -103,14 +103,19 @@ export default {
     margin-right: 4px;
   }
 /* スマホ表示用 */
-  .experience__location__sm{
+  .experience__info__sm{
     margin: 11px 10px 0;
     border: 1px solid #ccc;
     color: #333333;
     display: flex;
   }
-  .location__info__sm{
-    width: 100%;
+  .experience__info__sm:not(:first-child){
+    margin-top: 0px;
+    border-top: 0px;
+  }
+  .experience__info__left__sm{
+    height: 100%;
+    width: calc((100vw - 20px) * 0.4);
     padding: 8px 0 0 8px;
     font-size: 12px;
     border-right: 1px solid #ccc;
