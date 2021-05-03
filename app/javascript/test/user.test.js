d3e7074e9df5281a49bb0d0e4fe3607d7a56e78b
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-05-04 02:09:26 +0900

    add: 'store/user.js'のメールアドレス登録確認機能のテストコードを実装

--- app/javascript/test/user.test.js
+++ app/javascript/test/user.test.js
@@ -1,9 +1,36 @@
-import Vuex from 'vuex';
-import user from '../store/modules/user';
-import { createLocalVue } from '@vue/test-utils';
+import { createLocalVue } from '@vue/test-utils'
+import axios from 'axios';
+import Vuex from 'vuex'
+import user from '../store/modules/user'
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+
+// jest.mock('axios')
+const res = [
+  {
+    id: 1,
+    title: 'リスト1',
+    description: 'lorem ipsum',
+    done: true
+  },
+  {
+    id: 2,
+    title: 'リスト2',
+    description: 'lorem ipsum',
+    done: false
+  },
+  {
+    id: 3,
+    title: 'リスト3',
+    description: 'lorem ipsum',
+    done: true
+  }
+]
+
+jest.mock('axios')
+
+
 
 describe('store/user.js', () => {
   let store
@@ -60,8 +87,13 @@ describe('store/user.js', () => {
     test('ユーザー登録をする：userSignUp', () => {
 
     })
-    test('メールアドレスが登録済みか確認する：isRegisterd', () => {
+    test('メールアドレスが登録済みか確認する：isRegisterd', async () => {
+      const email = 'guest@sample.com'
+      axios.post.mockResolvedValue({ data: true })
+      const response = await user.actions['isRegisterd']({}, email)
 
+      expect(axios.post).toHaveBeenCalledWith('/api/v1/auth/check_email', { email: email })
+      expect(response).toStrictEqual({ data: true })
     })
     test('ユーザーをログインさせる：userLogin', () => {
 
