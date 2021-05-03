7de73402f5c54eefa22a9c8cc8d89a95c2121361
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-05-03 23:58:12 +0900

    'store/user.js'の'getters用テストコード'を実装 #164

--- app/javascript/test/user.test.js
+++ app/javascript/test/user.test.js
@@ -14,19 +14,42 @@ describe('store/user.js', () => {
 
   describe('getters', () => {
     test('ユーザー認証用トークンを取得：userTokens', () => {
+      const userTokens = {
+        'access-token': null,
+        'client': null,
+        'uid': null,
+      }
 
+      expect(store.getters['userTokens']).toEqual(userTokens)
     })
     test('ログインユーザー情報を取得：userData', () => {
+      const userData = {
+        id: null,
+        name: null,
+        email: null,
+        introduce: null,
+        admin: null,
+        favorites_counts: null,
+        reviews_counts: null,
+        histories_counts: null,
+      }
 
+      expect(store.getters['userData']).toEqual(userData)
     })
     test('ログインユーザーの口コミ情報を取得：userReviews', () => {
+      const userReviews = null
 
+      expect(store.getters['userReviews']).toEqual(userReviews)
     })
     test('ログインユーザーのいいね一覧を取得：userFavorites', () => {
+      const userFavorites = null
 
+      expect(store.getters['userFavorites']).toEqual(userFavorites)
     })
     test('ログインユーザーの行った一覧を取得：userHistories', () => {
+      const userHistories = null
 
+      expect(store.getters['userHistories']).toEqual(userHistories)
     })
   })
 
