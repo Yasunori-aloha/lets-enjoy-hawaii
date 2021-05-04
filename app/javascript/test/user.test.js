ccbac33d51dfa1c29e67704c29e054c300020573
Yasunori-arloha <gyarubaku@yahoo.ne.jp>
2021-05-05 01:40:51 +0900

    add: 'store/user.js'のユーザー新規登録時のテストコードを実装 #164

--- app/javascript/test/user.test.js
+++ app/javascript/test/user.test.js
@@ -6,31 +6,7 @@ import user from '../store/modules/user'
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
-// jest.mock('axios')
-const res = [
-  {
-    id: 1,
-    title: 'リスト1',
-    description: 'lorem ipsum',
-    done: true
-  },
-  {
-    id: 2,
-    title: 'リスト2',
-    description: 'lorem ipsum',
-    done: false
-  },
-  {
-    id: 3,
-    title: 'リスト3',
-    description: 'lorem ipsum',
-    done: true
-  }
-]
-
 jest.mock('axios')
-
-
 
 describe('store/user.js', () => {
   let store
@@ -84,7 +60,42 @@ describe('store/user.js', () => {
     test('ユーザー認証用トークンがあれば自動ログインをする：autoLogin', () => {
 
     })
-    test('ユーザー登録をする：userSignUp', () => {
+    test('ユーザーを新規登録して、VuexとLocalStorageにデータを格納する：userSignUp', async () => {
+      const commit = store.commit
+      const signUpForms = [
+        { input: 'テストユーザー' },
+        { input: 'guest@sample.com' },
+        { input: 'testpassword' },
+        { input: 'testpassword' },
+      ]
+      const userTokens = {
+        'access-token': 'testAccessToken',
+        'client': 'testClentToken',
+        'uid': 'testUidToken',
+      }
+      const userData = {
+        id: '1',
+        name: 'テストユーザー',
+        email: 'guest@sample.com',
+        introduce: '',
+        admin: 'false',
+        favorites_counts: '0',
+        reviews_counts: '0',
+        histories_counts: '0',
+      }
+      axios.post.mockResolvedValue({ headers: userTokens, data: userData })
+      await user.actions['userSignUp']({ commit }, signUpForms)
+
+      expect(store.getters['userTokens']).toEqual(userTokens)
+      expect(store.getters['userData']).toEqual(userData)
+      expect(window.localStorage.getItem('id')).toBe(userData['id'])
+      expect(window.localStorage.getItem('name')).toBe(userData['name'])
+      expect(window.localStorage.getItem('email')).toBe(userData['email'])
+      expect(window.localStorage.getItem('introduce')).toBe(userData['introduce'])
+      expect(window.localStorage.getItem('admin')).toBe(userData['admin'])
+      expect(window.localStorage.getItem('access-token')).toBe(userTokens['access-token'])
+      expect(window.localStorage.getItem('client')).toBe(userTokens['client'])
+      expect(window.localStorage.getItem('uid')).toBe(userTokens['uid'])
 
     })
     test('メールアドレスが登録済みか確認する：isRegisterd', async () => {
